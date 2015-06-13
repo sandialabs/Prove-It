e76816cd256f30f26ba8f77a2fb01d8025c42e7a
Wayne Witzel <wwitzel@sandia.gov>
2015-06-13 07:20:25 -0600

    A simple List Operation that is nestable (unlike an ExpressionList).

---
+++ basiclogic/lists.py
@@ -0,0 +1,31 @@
+import sys
+from proveit.statement import *
+from proveit.context import *
+from genericOperations import *
+from variables import *
+
+# set theory related literals
+literals = Literals()
+LIST = literals.add('LIST')
+
+def _defineAxioms():        
+    return None
+
+def _defineTheorems():
+    return None
+        
+lists = Context(sys.modules[__name__], literals, _defineAxioms, _defineTheorems)
+
+class List(Operation):
+    '''
+    Defines a list of items (expressions).  Unlike ExpressionList in the proveit core, this
+    kind of List is nestable.
+    '''
+    
+    def __init__(self, *items):
+        Operation.__init__(self, LIST, items)
+
+    def formatted(self, formatType, fenced=False):
+        return '[' + self.operand.formatted(formatType, fenced=False) + ']'
+
+Operation.registerOperation(LIST, lambda operands : List(*operands))
