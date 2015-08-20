c7b14bb334cd3fa325e9509c1242aebbd94d613e
Wayne Witzel <wwitzel@sandia.gov>
2015-08-20 09:59:50 -0600

    getting this to work with multiline definitions

--- notebook_convert_helper.py
+++ notebook_convert_helper.py
@@ -4,13 +4,15 @@ import re
 filename = sys.argv[1]
 
 with open(filename) as f:
+    lhs = None
     for line in f.readlines():
         line = '\n' + line
+        if line.strip() == '' and lhs is not None:
+            print lhs + '\n\n' # print the previous lhs
         try:
             before, lhs, rhs = re.split('\n(\S*)\s*=', line)
             if before == '':
                 print lhs + ' =' + rhs.rstrip()
-                print lhs + '\n'
         except:
             pass
         
