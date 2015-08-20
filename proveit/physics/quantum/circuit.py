140ecbcea33e2f36cd30ba274be7fe79d451d6b7
Wayne Witzel <wwitzel@sandia.gov>
2015-08-20 15:00:22 -0600

    working on quantum circuits and quantum phase estimation algorithm

--- proveit/physics/quantum/circuit.py
+++ proveit/physics/quantum/circuit.py
@@ -1,38 +1,39 @@
 import sys
-from proveit.statement import Literals, Operation, Variable, MultiVariable, Lambda, ExpressionList, STRING, LATEX
-from proveit.context import Context
-from proveit.basiclogic import Forall, In, List, Equals
-from proveit.computer_science.regular_expressions import KleeneRepetition
-from Carbon import ControlAccessor
+from proveit.expression import Literal, Operation, Lambda, STRING, LATEX
+from proveit.multiExpression import ExpressionTensor, Block
+from proveit.basiclogic import Forall, In, Equals
+#from proveit.computer_science.regular_expressions import KleeneRepetition
 
-# quantum circuit literals
-literals = Literals()
-GATE = literals.add('GATE')
-CIRCUIT = literals.add('CIRCUIT')
-I = literals.add('I') # Single qubit identity
-X = literals.add('X') # Pauli-X
-Y = literals.add('Y') # Pauli-Y
-Z = literals.add('Z') # Pauli-Z
-H = literals.add('H') # Hadamard
-CTRL_UP = literals.add('CTRL_UP')
-CTRL_DN = literals.add('CTRL_DN')
-CTRL_UPDN = literals.add('CTRL_UPDN')
+pkg = __package__
 
+# quantum circuit gate literals
+I = Literal(pkg, 'I') # Single qubit identity
+X = Literal(pkg, 'X') # Pauli-X
+Y = Literal(pkg, 'Y') # Pauli-Y
+Z = Literal(pkg, 'Z') # Pauli-Z
+H = Literal(pkg, 'H') # Hadamard
+CTRL_UP = Literal(pkg, 'CTRL_UP')
+CTRL_DN = Literal(pkg, 'CTRL_DN')
+CTRL_UPDN = Literal(pkg, 'CTRL_UPDN')
+TARGET = Literal(pkg, 'TARGET', {STRING:'TARGET', LATEX:r'\targ'})
+
+"""
 # A BARRIER is required to separate independent gates that operate in parallel
 # and are adjacent in a quantum circuit (e.g., no controlled gate between them):
 BARRIER = literals.add('BARRIER', {STRING:'|', LATEX:'|'})
+"""
 
-class ImplicitIdentities(MultiVariable):
+class ImplicitIdentities(Block):
     '''
     ImplicitIdentities are used in quantum circuits where they must be
     filled in with one or more identities as determined by the width of
-    the circuit (which isn't established until MultiVariables are specialized).
+    the circuit (which isn't established until Blocks are specialized).
     See ForallWithImplicitIdentities.
     '''
-    
     def __init__(self, name, formatMap = None):
-        MultiVariable.__init__(self, name, formatMap)
-    
+        Block.__init__(self, name, formatMap)
+
+"""    
 def areIdentities(gates):
     '''
     Returns the expression that the set of gates, as a List, are in the set of repeating identities.
@@ -40,42 +41,127 @@ def areIdentities(gates):
     '''
     return In(List(gates), KleeneRepetition(I))
 
-# some Variable labels
-multiA = MultiVariable('A')
-multiB = MultiVariable('B')
-multiB1 = MultiVariable('B1')
-multiB2 = MultiVariable('B2')
-multiB3 = MultiVariable('B3')
-multiC = MultiVariable('C')
-multiC1 = MultiVariable('C1')
-multiC2 = MultiVariable('C2')
-multiC3 = MultiVariable('C3')
-multiD = MultiVariable('D')
-# for implicit identity gates
-Is = ImplicitIdentities('I') 
-IsB = ImplicitIdentities('IB') 
-IsC = ImplicitIdentities('IC') 
+
 
 def _defineAxioms():
     return None # For now, we're just asserting the theorems without proof.
 
 def _defineTheorems():
     _firstTheorem = \
-    identityCollapse = ForallWithImplicitIdentities([multiA, multiB, Is], Equals(Circuit(multiA, Gates(Is), multiB), Circuit(multiA, multiB)))
-    reverseCzDn = ForallWithImplicitIdentities([multiA, multiB, multiC, multiD, Is], Equals(Circuit(multiA, Gates(multiB, Z, Is, CTRL_UP, multiC), multiD), 
-                                                                                            Circuit(multiA, Gates(multiB, CTRL_DN, Is, Z, multiC), multiD)))
-    reverseCzUp = ForallWithImplicitIdentities([multiA, multiB, multiC, multiD, Is], Equals(Circuit(multiA, Gates(multiB, CTRL_DN, Is, Z, multiC), multiD), 
-                                                                                            Circuit(multiA, Gates(multiB, Z, Is, CTRL_UP, multiC), multiD)))
-    reverseCnotDnToUp = ForallWithImplicitIdentities([multiA, multiB, multiC, multiD, Is], 
-                                                     Equals(Circuit(multiA, Gates(multiB, CTRL_DN, Is, X, multiC), multiD), 
-                                                            Circuit(multiA, Gates(IsB, H, Is, H, IsC), Gates(multiB, X, Is, CTRL_UP, multiC2), Gates(IsB, H, Is, H, IsC), multiD)))
-    reverseCnotUpToDn = ForallWithImplicitIdentities([multiA, multiB, multiC, multiD, Is], 
-                                                     Equals(Circuit(multiA, Gates(multiB, X, Is, CTRL_UP, multiC), multiD), 
-                                                            Circuit(multiA, Gates(IsB, H, Is, H, IsC), Gates(multiB, CTRL_DN, Is, X, multiC2), Gates(IsB, H, Is, H, IsC), multiD)))
+    identityCollapse = ForallWithImplicitIdentities([Aetc, multiB, Is], Equals(Circuit(Aetc, Gates(Is), multiB), Circuit(Aetc, multiB)))
+    reverseCzDn = ForallWithImplicitIdentities([Aetc, multiB, Cetc, multiD, Is], Equals(Circuit(Aetc, Gates(multiB, Target(Z), Is, CTRL_UP, Cetc), multiD), 
+                                                                                            Circuit(Aetc, Gates(multiB, CTRL_DN, Is, Target(Z), Cetc), multiD)))
+    reverseCzUp = ForallWithImplicitIdentities([Aetc, multiB, Cetc, multiD, Is], Equals(Circuit(Aetc, Gates(multiB, CTRL_DN, Is, Target(Z), Cetc), multiD), 
+                                                                                            Circuit(Aetc, Gates(multiB, Target(Z), Is, CTRL_UP, Cetc), multiD)))
+    reverseCnotDnToUp = ForallWithImplicitIdentities([Aetc, multiB, Cetc, multiD, Is, IsB, IsC], 
+                                                     Equals(Circuit(Aetc, Gates(multiB, CTRL_DN, Is, Target(X), Cetc), multiD), 
+                                                            Circuit(Aetc, Gates(IsB, H, Is, H, IsC), Gates(multiB, Target(X), Is, CTRL_UP, Cetc), Gates(IsB, H, Is, H, IsC), multiD)))
+    reverseCnotUpToDn = ForallWithImplicitIdentities([Aetc, multiB, Cetc, multiD, Is, IsB, IsC], 
+                                                     Equals(Circuit(Aetc, Gates(multiB, Target(X), Is, CTRL_UP, Cetc), multiD), 
+                                                            Circuit(Aetc, Gates(IsB, H, Is, H, IsC), Gates(multiB, CTRL_DN, Is, Target(X), Cetc), Gates(IsB, H, Is, H, IsC), multiD)))
     return _firstTheorem, locals()
-
-circuit = Context(sys.modules[__name__], literals, _defineAxioms, _defineTheorems)
+"""
         
+class Target(Operation):
+    '''
+    Represents the target of a control.
+    '''
+    
+    def __init__(self, target_gate):
+        '''
+        Create a Target operation with the given target_gate as the type
+        of the gate for the target (e.g., X for CNOT and Z for Controlled-Z).
+        '''    
+        Operation.__init__(self, TARGET, target_gate)
+            
+
+
+class Circuit(Operation):
+    '''
+    Represents a quantum circuit as a 2-D ExpressionTensor.
+    '''
+    def __init__(self, tensor):
+        '''
+        Create an Circuit either with a dense tensor (list of lists ... of lists) or
+        with a dictionary mapping pairs of indices to Expression elements or blocks,
+        composing an ExpressionTensor.
+        '''
+        Operation.__init__(self, CIRCUIT, tensor)
+        if not isinstance(tensor, dict):
+            # remove identity gates -- these are implicit
+            tensor, _ = ExpressionTensor._tensor_from_iterables(tensor)
+            for idx in tensor.keys():
+                if tensor[idx] == I:
+                    tensor.pop(idx)
+        if not isinstance(self.operands, ExpressionTensor):
+            raise TypeError('Circuit operand must be an ExpressionTensor')
+        if len(self.operands.shape) != 2:
+            raise ValueError('Circuit operand must be a 2-D ExpressionTensor')
+    
+    def _config_latex_tool(self, lt):
+        Operation._config_latex_tool(self, lt)
+        if not 'qcircuit' in lt.packages:
+            lt.packages.append('qcircuit')
+    
+    def nearestTarget(self, row, column, dir):
+        '''
+        Report the vertical distance between (row, column) and
+        the nearest Target in the given direction (dir < 0 for up
+        and dir > 0 for down).  Raise an exception if there is
+        anything in between (row, column) and the Target.
+        '''
+        r = row + dir
+        while not (r, column) in self.operands:
+            r += dir
+            if r < 0 or r >= self.shape[1]:
+                raise QuantumCircuitException('Control with no target at (%d, %d)'%(row, column))                
+        #if not isinstance(self.operands[r, column], Target):
+        #    raise QuantumCircuitException('There must be no non-identity gate between a control and target')
+        return r - row
+        
+    def formatted(self, formatType, fence=False):
+        return ''.join(self.formatter(formatType, fence))
+    
+    def formatter(self, formatType, fence=False):
+        if formatType == LATEX:
+            if fence: yield r'\left[' + '\n'
+            yield r'\begin{array}{c}' + '\n'
+            yield r'\Qcircuit @C=1em @R=.7em {' # + '\n'
+            nrows, ncolumns = self.operands.shape
+            for row in xrange(nrows):
+                if row > 0: yield r' \\ ' #+ '\n'
+                for column in xrange(ncolumns+1):
+                    if (row, column) not in self.operands:
+                        yield r' & \qw' # a quantum wire is the default
+                    else:
+                        gate = self.operands[row, column]
+                        if gate == CTRL_UP:
+                            yield r' & \ctrl{' + str(self.nearestTarget(row, column, -1)) + '}'
+                        elif gate == CTRL_DN:
+                            yield r' & \ctrl{' + str(self.nearestTarget(row, column, 1)) + '}'
+                        elif gate == CTRL_UPDN:
+                            yield r' & \ctrl{' + str(self.nearestTarget(row, column, -1)) + '}'
+                            yield r' \qwx[' + str(self.nearestTarget(row, column, 1)) + ']'
+                        elif gate == TARGET:
+                            yield r' & ' + self.operands[row, column].formatted(formatType, False)
+                        else:
+                            yield r' & \gate{' + self.operands[row, column].formatted(formatType, False) + '}'
+            yield '}\n'
+            yield r'\end{array}'
+            if fence: yield '\n' + r'\right]'
+        else:
+            yield Operation.formatted(self, formatType, fence)
+    
+CIRCUIT = Literal(pkg, 'CIRCUIT', operationMaker = lambda operands : Circuit(operands))
+                
+
+class QuantumCircuitException():
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return self.msg
+    
+""" 
 class Gates(Operation):
     '''
     Represents a column of gate operations in parallel on one or more qubits.
@@ -83,7 +169,7 @@ class Gates(Operation):
     
     def __init__(self, *gates):
         Operation.__init__(self, GATE, gates)
-        self.gates = gates = self.operand
+        self.gates = gates = self.etcExpr
         self.gate_min_widths = [gate.size if hasattr(gate, 'width') else 1 for gate in gates]
         self.min_nrows = sum(self.gate_min_widths)
         self.multivar_rows = {row for row, gate in enumerate(gates) if isinstance(gate, MultiVariable)}        
@@ -128,6 +214,8 @@ class Gates(Operation):
             target_idx = idx+direction
             while self.gate_by_minrow[target_idx] == I or isinstance(self.gate_by_minrow[target_idx], ImplicitIdentities):
                 target_idx += direction
+            target = self.get_by_minrow[target_idx]
+            assert set([gate, target]) == set([CTRL_DN, CTRL_UP]) or isinstance(target, Target), "There may only be identities between a control and target"
             if gate == CTRL_DN and self.gate_by_minrow[target_idx] == CTRL_UP:
                 return gate, 'control' # one end of a CPhase with control at either end (equivalent, but not represented the same as, a controlled-Z)
             else:
@@ -139,11 +227,11 @@ class Gates(Operation):
         elif gate == I: return gate, '\qw' # Identity is just a quantum wire
         else: return gate, 'gate'
          
-    def formatted(self, formatType, fenced=False, circuit=None, row=None, column=None, multivar_row=False):
+    def formatted(self, formatType, fence=False, circuit=None, row=None, column=None, multivar_row=False):
         if formatType == LATEX:
             if row is None:
                 # present the whole -- as if it were a circuit with one column
-                return Circuit([self]).formatted(formatType, fenced)
+                return Circuit([self]).formatted(formatType, fence)
             else:
                 gate, gate_type = self.gate_and_type(circuit, row)
                 if multivar_row: 
@@ -155,7 +243,7 @@ class Gates(Operation):
                 else:
                     return ' \\' + gate_type # e.g., \qw, \control, \ctrl{#}
         else:
-            return Operation.formatted(self, formatType, fenced)
+            return Operation.formatted(self, formatType, fence)
         
 Operation.registerOperation(GATE, lambda operands : Gates(*operands))
 
@@ -192,10 +280,10 @@ class Circuit(Operation):
     def hasFixedWidth(self):
         return hasattr(self, 'width')
 
-    def formatted(self, formatType, fenced=False):
+    def formatted(self, formatType, fence=False):
         if formatType == LATEX:
             outStr = '\n'
-            if fenced: outStr += r'\left['
+            if fence: outStr += r'\left['
             outStr += r'\begin{array}{c}' + '\n' + r'\Qcircuit @C=1em @R=.7em {'
             for row in xrange(self.nrows):
                 if row > 0: outStr += r'\\' + '\n'
@@ -207,16 +295,16 @@ class Circuit(Operation):
                             outStr += r'& \gate' + postfix # A multi-variable column on just one row
                         else:
                             outStr += '& ' + (r'\multigate' + postfix + '{' + str(self.nrows-1) + '}' if row == 0 else r'\ghost' + postfix)
-                        outStr += '{' + column.formatted(formatType, fenced) + '} '
+                        outStr += '{' + column.formatted(formatType, fence) + '} '
                         # Draw a dotted box around MultiVariable columns to distinguish them from a Gates with a solo MultiVariable.
                         #outStr += r'\gategroup{1}{' + str(k+1) + '}{'+ str(self.nrows) + '}{' + str(k+2) + '}{.}{.5em}'
                     else:
                         outStr += '&' + column.formatted(LATEX, circuit=self, row=row, column=k, multivar_row=(row in self.multivar_rows))
             outStr += '}\n' + r'\end{array}' + '\n'
-            if fenced: outStr += r'\right]'
+            if fence: outStr += r'\right]'
             return outStr
         else:
-            return Operation.formatted(self, formatType, fenced)
+            return Operation.formatted(self, formatType, fence)
         
     def controlReversal(self, row, col):
         '''
@@ -237,13 +325,13 @@ class Circuit(Operation):
         multiD_val = column.gates[max(row, target_row):]
         target = column.gates[target_row]
         if target == Z and control_type == CTRL_DN:
-            return circuit.reverseCzDn.specialize({multiA:multiA_val, multiB:multiB_val, multiC:multiC_val, multiD:multiD_val})
+            return circuit.reverseCzDn.specialize({Aetc:multiA_val, multiB:multiB_val, Cetc:multiC_val, multiD:multiD_val})
         elif target == Z and control_type == CTRL_UP:
-            return circuit.reverseCzUp.specialize({multiA:multiA_val, multiB:multiB_val, multiC:multiC_val, multiD:multiD_val})
+            return circuit.reverseCzUp.specialize({Aetc:multiA_val, multiB:multiB_val, Cetc:multiC_val, multiD:multiD_val})
         elif target == X and control_type == CTRL_DN:
-            return circuit.reverseCnotDnToUp.specialize({multiA:multiA_val, multiB:multiB_val, multiC:multiC_val, multiD:multiD_val})
+            return circuit.reverseCnotDnToUp.specialize({Aetc:multiA_val, multiB:multiB_val, Cetc:multiC_val, multiD:multiD_val})
         elif target == X and control_type == CTRL_UP:
-            return circuit.reverseCnotUpToDn.specialize({multiA:multiA_val, multiB:multiB_val, multiC:multiC_val, multiD:multiD_val})
+            return circuit.reverseCnotUpToDn.specialize({Aetc:multiA_val, multiB:multiB_val, Cetc:multiC_val, multiD:multiD_val})
         
 
 Operation.registerOperation(CIRCUIT, lambda operands : Circuit(*operands))
@@ -261,13 +349,13 @@ class ForallWithImplicitIdentities(Forall):
     method.
     '''
     
-    def __init__(self, instanceVars, instanceExpression, conditions=None):
+    def __init__(self, instanceVars, instanceExpr, conditions=None):
         '''
         Create a special Forall expression with ImplicitIdentities as one or
         more of the instanceVars.  Adds appropriate conditions that restrict
         these to be specialized as one or more identities.
         '''
-        Forall.__init__(self, instanceVars, instanceExpression, conditions=ForallWithImplicitIdentities._with_implicit_conditions(instanceVars, conditions))
+        Forall.__init__(self, instanceVars, instanceExpr, conditions=ForallWithImplicitIdentities._with_implicit_conditions(instanceVars, conditions))
         # Extract the ImplicitIdentities
         self.implicit_identities = {var for var in instanceVars if isinstance(var, ImplicitIdentities)}
         # Extract the conditions involving ImplicitIdentities
@@ -300,14 +388,14 @@ class ForallWithImplicitIdentities(Forall):
         Automatically sets the ImplicitIdentities if the other specializations
         cause the width of the quantum circuit to be determined.
         '''
-        subbed_expr = self.instanceExpression.substituted(subMap)
+        subbed_expr = self.instanceExpr.substituted(subMap)
         def fixImplicitIdentityWidths(expr):
             if not isinstance(expr, Circuit):
                 if isinstance(expr, ExpressionList):
                     for subexpr in expr:
                         fixImplicitIdentityWidths(subexpr) # recurse over an ExpressionList
                 if isinstance(expr, Operation):
-                    fixImplicitIdentityWidths(expr.operand) # recursively search for Circuit subexpression
+                    fixImplicitIdentityWidths(expr.etcExpr) # recursively search for Circuit subexpression
                     fixImplicitIdentityWidths(expr.operator) # what the heck, try all the sub expressions
                 elif isinstance(expr, Lambda):
                     fixImplicitIdentityWidths(expr.expression)
@@ -324,7 +412,7 @@ class ForallWithImplicitIdentities(Forall):
         fixImplicitIdentityWidths(subbed_expr)
         return Forall.specialize(self, subMap)
             
-            
+"""            
         
-        
+
         
