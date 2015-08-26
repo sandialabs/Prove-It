efc4567ed37c43b17e56c5aa8487416fa336f101
Kenny Rudinger <kmrudin@sandia.gov>
2015-08-26 16:35:53 -0600

    Updated proveit.number.__init__.py to import all classes from arithmeticOps.py

--- proveit/number/__init__.py
+++ proveit/number/__init__.py
@@ -1,2 +1,3 @@
-#from arithmetic import *
-#from integers import *
+from proveit.number.arithmeticOps import DiscreteContiguousSet, Interval, IntervalOO, IntervalOC, IntervalCO, IntervalCC
+from proveit.number.arithmeticOps import Ket, OrderingRelation, LessThan, LessThanEquals, GreaterThan, GreaterThanEquals, Pr
+from proveit.number.arithmeticOps import Abs, Add, Subtract, Multiply, Divide, Fraction, Exponentiate, Summation, Neg, Integrate
