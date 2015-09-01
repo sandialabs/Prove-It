f10c40cc3fc605da34b504a1152586d4e3373d01
Wayne Witzel <wwitzel@sandia.gov>
2015-09-01 09:42:56 -0600

    A start on a trigonometry package (just what we need for now).

---
+++ proveit/trigonometry/trigOps.py
@@ -0,0 +1,11 @@
+from proveit.expression import Literal, Operation, LATEX
+
+pkg = __package__
+
+class AngDiff(Operation):
+    def __init__(self, angle1, angle2):
+        Operation.__init__(self, ANGULAR_DIFFERENCE, (angle1, angle2))
+        self.angle1 = angle1
+        self.angle2 = angle2
+
+ANGULAR_DIFFERENCE = Literal(pkg, 'AngDiff', {LATEX:r'{\rm AngDiff}'}, operationMaker = lambda operands : AngDiff(*operands))
