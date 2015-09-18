e09244b71cd312dc0dd1ed8a88430df57bbb5e3e
Wayne Witzel <wwitzel@sandia.gov>
2015-09-17 18:25:01 -0600

    added extractSubExpression

--- proveit/basiclogic/__init__.py
+++ proveit/basiclogic/__init__.py
@@ -3,6 +3,6 @@ from boolean.quantifiers import Forall, Exists, NotExists
 from boolean.boolSet import TRUE, FALSE, BOOLEANS, inBool, deduceInBool
 from boolean.boolOps import And, Or, Not, Implies, Iff, deriveStmtEqTrue, compose
 from set.setOps import In, NotIn, Singleton, Complement, Union, Intersection, Difference, SubsetEq, SupersetEq, SetOfAll, EVERYTHING, NOTHING
-from equality.eqOps import Equals, NotEquals, autoSubstitute, autoSubstitution, autoStatementSubstitution, extractSubExpr
+from equality.eqOps import Equals, NotEquals, autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
 from equality.equation import Equation
 from mapping.mappingOps import Domain, CoDomain
