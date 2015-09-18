f56fa66169e91296cd9a3013b8967514e45db3d1
Wayne Witzel <wwitzel@sandia.gov>
2015-09-17 18:25:04 -0600

    added extractSubExpression

--- proveit/basiclogic/equality/__init__.py
+++ proveit/basiclogic/equality/__init__.py
@@ -1,2 +1,2 @@
-from eqOps import Equals, NotEquals, autoSubstitute, autoSubstitution, autoStatementSubstitution, extractSubExpr
+from eqOps import Equals, NotEquals, autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
 from equation import Equation
