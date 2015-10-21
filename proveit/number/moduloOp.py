a62e57e9edde6cc9ff7b5682b420f60889a7d238
Wayne Witzel <wwitzel@sandia.gov>
2015-10-20 18:39:06 -0600

    Defined ModAbs

--- proveit/number/moduloOp.py
+++ proveit/number/moduloOp.py
@@ -1,5 +1,5 @@
 from proveit.expression import Literal, STRING, LATEX
-from proveit.basiclogic.genericOps import BinaryOperation
+from proveit.basiclogic.genericOps import Operation, BinaryOperation
 from numberSets import NumberOp, Reals, Integers
 
 pkg = __package__
@@ -19,3 +19,22 @@ class Mod(BinaryOperation, NumberOp):
             return real.theorems.modClosure
 
 MOD = Literal(pkg, 'MOD', {STRING:'mod', LATEX:r'~\rm{mod}~'}, operationMaker = lambda operands : Mod(*operands))
+
+class ModAbs(Operation, NumberOp):
+    def __init__(self, value, divisor):
+        Operation.__init__(self, MOD_ABS, [value, divisor])
+        self.value = value
+        self.divisor = divisor
+    
+    def formatted(self, formatType, fence=False):
+        if formatType == STRING:
+            return '|'+self.value.formatted(formatType, fence=fence)+'|_{mod ' + self.divisor.formatted(formatType, fence=fence) + '}'
+        elif formatType == LATEX:
+            return r'\left|'+self.value.formatted(formatType, fence=fence)+r'\right|_{{\rm mod}~' + self.divisor.formatted(formatType, fence=fence) + '}'
+
+    def _closureTheorem(self, numberSet):
+        import real.theorems
+        if numberSet == Reals:
+            return real.theorems.modAbsClosure
+
+MOD_ABS = Literal(pkg, 'MOD_ABS', operationMaker = lambda operands : ModAbs(*operands))
