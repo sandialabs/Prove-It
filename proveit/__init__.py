521654f1da7eb261ea94cc3a14c222a4128b5527
Wayne Witzel <wwitzel@sandia.gov>
2016-05-16 21:37:05 -0600

    reorganized core.

--- proveit/__init__.py
+++ proveit/__init__.py
@@ -1,2 +1,7 @@
-from proveit import expression
+from core import Expression, Operation, Lambda, Label, Variable, MultiVariable, Literal, Etcetera, Block, safeDummyVar
+from core import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression
+from core import Statement, Prover
+
+# Implies, Forall, and InSet are core concepts that are defined outside of the core.
+from proveit.logic import Implies, Forall, InSet
 
