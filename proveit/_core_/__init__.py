9e67be5f8f05b783cc01e5104ed0efe578337803
Wayne Witzel <wwitzel@sandia.gov>
2016-06-06 08:47:35 -0600

    Added specialStatements methods and an exception class

--- proveit/_core_/__init__.py
+++ proveit/_core_/__init__.py
@@ -1,5 +1,6 @@
 from expression import Expression, Operation, Lambda, Label, Variable, MultiVariable, Literal, Etcetera, Block, safeDummyVar
 from expression import MakeNotImplemented, ImproperRelabeling, ImproperSubstitution, ScopingViolation, ProofFailure
-from expression import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression
+from expression import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
 from statement import Statement
+from specialStatements import beginAxioms, endAxioms, beginTheorems, endTheorems
 from prover import Prover
