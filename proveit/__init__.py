05e08c58106fc9963e4f0e4fa231ceb00743de90
Wayne Witzel <wwitzel@sandia.gov>
2016-06-06 08:50:21 -0600

    Updating with _generic_ classes, specialStatements, exception class(es).

--- proveit/__init__.py
+++ proveit/__init__.py
@@ -1,7 +1,8 @@
 from _core_ import Expression, Operation, Lambda, Label, Variable, MultiVariable, Literal, Etcetera, Block, safeDummyVar
 from _core_ import MakeNotImplemented, ImproperRelabeling, ImproperSubstitution, ScopingViolation, ProofFailure
-from _core_ import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression
-from _core_ import Statement, Prover
+from _core_ import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
+from _core_ import Statement, Prover, beginAxioms, endAxioms, beginTheorems, endTheorems
+from _generic_ import BinaryOperation, AssociativeOperation, OperationOverInstances, InstanceSubstitutionException
 
 # Implies, Forall, and InSet are core concepts that are defined outside of the core.
 from proveit.logic import Implies, Forall, InSet
