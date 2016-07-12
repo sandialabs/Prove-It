e2365d31527513a5c07003c2607cfaf48ff75e56
Wayne Witzel <wwitzel@sandia.gov>
2016-07-12 06:38:34 -0600

    Added RelabelingFailure and maybeFenced...

--- proveit/__init__.py
+++ proveit/__init__.py
@@ -3,8 +3,9 @@ from _core_ import MakeNotImplemented, ImproperRelabeling, ImproperSubstitution,
 from _core_ import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
 from _core_ import beginAxioms, endAxioms, beginTheorems, endTheorems, KnownTruth
 from _core_ import Proof, Assumption, Axiom, Theorem, ModusPonens, HypotheticalReasoning, Specialization, Generalization
-from _core_ import ModusPonensFailure, SpecializationFailure, GeneralizationFailure
+from _core_ import ModusPonensFailure, RelabelingFailure, SpecializationFailure, GeneralizationFailure
 from _generic_ import BinaryOperation, AssociativeOperation, OperationOverInstances, InstanceSubstitutionException
+from _generic_ import maybeFencedString, maybeFencedLatex, maybeFenced
 
 # Implies, Forall, and InSet are core concepts that are defined outside of the core.
 from proveit.logic import Implies, Forall, InSet
