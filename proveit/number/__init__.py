1200c1b55a1b5f92e50fcd214cd35cbc31b4e8ed
Wayne Witzel <wwitzel@sandia.gov>
2016-07-13 20:59:29 -0600

    include number sets and num

--- proveit/number/__init__.py
+++ proveit/number/__init__.py
@@ -2,6 +2,9 @@ from proveit.number.arithmeticOps import DiscreteContiguousSet, Interval, Interv
 from proveit.number.arithmeticOps import OrderingRelation, LessThan, LessThanEquals, GreaterThan, GreaterThanEquals
 from proveit.number.arithmeticOps import Abs, Add, Subtract, Multiply, Divide, Fraction, Exponentiate, Sqrt, Summation, Neg, Integrate
 from proveit.number.arithmeticOps import Min, Max
+from proveit.number.numberSets import Naturals, NaturalsPos, Integers, Reals, RealsNeg, RealsPos, Complexes
 from proveit.number.moduloOp import Mod, ModAbs
 from proveit.number.roundingOps import Floor, Ceil, Round
 from proveit.number.number import num
+
+Pow = Exponentiate
