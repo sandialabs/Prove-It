76af16d6c7bb8c8fc49ea0b6f0bd779de99b7177
Wayne Witzel <wwitzel@sandia.gov>
2016-07-13 20:43:17 -0600

    split defaults and storage into their own files

--- proveit/_core_/__init__.py
+++ proveit/_core_/__init__.py
@@ -2,7 +2,8 @@ from expression import Expression, Operation, Lambda, Label, Variable, MultiVari
 from expression import MakeNotImplemented, ImproperRelabeling, ImproperSubstitution, ScopingViolation, ProofFailure
 from expression import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
 from proveit._core_.known_truth import KnownTruth
-from defaults_and_settings import defaults, USE_DEFAULTS, storage
+from proveit._core_.defaults import defaults, USE_DEFAULTS, InvalidAssumptions
+from proveit._core_.storage import storage
 from proveit._core_.special_statements import beginAxioms, endAxioms, beginTheorems, endTheorems
 from proof import Proof, Assumption, Axiom, Theorem, ModusPonens, HypotheticalReasoning, Specialization, Generalization
 from proof import ModusPonensFailure, RelabelingFailure, SpecializationFailure, GeneralizationFailure
