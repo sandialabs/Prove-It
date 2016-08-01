567d680a1fd992ded21407d2022d437f722f7d7e
Witzel <wwitzel@srn.sandia.gov>
2016-08-01 06:29:47 -0600

    Added transitivitySearch and removed Equation.

--- proveit/logic/__init__.py
+++ proveit/logic/__init__.py
@@ -3,6 +3,6 @@ from boolean import TRUE, FALSE, BOOLEANS, inBool, deduceInBool
 from boolean import Forall, Exists, NotExists
 from set_theory import InSet, NotInSet, Singleton, Union, Intersect, Difference, SubsetEq, SupersetEq, SetOfAll
 from set_theory.common import NOTHING
-from equality import Equals, NotEquals, autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
-from equality import Equation
+from equality import Equals, NotEquals, transitivitySearch
+from equality import autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
 #from mapping.mappingOps import Domain, CoDomain
