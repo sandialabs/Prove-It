6b12e7f1991450a09dbb9ca0a9aa81d50028127c
Witzel <wwitzel@srn.sandia.gov>
2016-08-01 06:20:17 -0600

    Added asExpression and asExpressions.

--- proveit/_core_/__init__.py
+++ proveit/_core_/__init__.py
@@ -1,7 +1,7 @@
 from expression import Expression, Operation, Lambda, Label, Variable, MultiVariable, Literal, Etcetera, Block, safeDummyVar
 from expression import MakeNotImplemented, ImproperRelabeling, ImproperSubstitution, ScopingViolation, ProofFailure
 from expression import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
-from proveit._core_.known_truth import KnownTruth
+from proveit._core_.known_truth import KnownTruth, asExpression, asExpressions
 from proveit._core_.defaults import defaults, USE_DEFAULTS, InvalidAssumptions
 from proveit._core_.storage import storage
 from proveit._core_.special_statements import beginAxioms, endAxioms, beginTheorems, endTheorems
