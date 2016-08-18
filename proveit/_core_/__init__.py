2a585cbeeba53ed558fbc302dd6107a1f17cd08c
Wayne Witzel <wwitzel@sandia.gov>
2016-08-17 21:07:52 -0600

    Added Composite

--- proveit/_core_/__init__.py
+++ proveit/_core_/__init__.py
@@ -1,6 +1,6 @@
 from expression import Expression, Operation, Lambda, Label, Variable, MultiVariable, Literal, Etcetera, Block, safeDummyVar
 from expression import MakeNotImplemented, ImproperRelabeling, ImproperSubstitution, ScopingViolation, ProofFailure
-from expression import ExpressionList, ExpressionTensor, NamedExpressions, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
+from expression import ExpressionList, ExpressionTensor, NamedExpressions, Composite, compositeExpression, singleOrCompositeExpression, NestedCompositeExpressionError
 from proveit._core_.known_truth import KnownTruth, asExpression, asExpressions
 from proveit._core_.defaults import defaults, USE_DEFAULTS, InvalidAssumptions
 from proveit._core_.storage import storage
