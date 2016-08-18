d52bf29cb438967ac1ec868cad0c93a5d75c226f
Wayne Witzel <wwitzel@sandia.gov>
2016-08-17 21:04:16 -0600

    BOOLEANS -> Booleans and removed equality.autoSubstitute, etc methods

--- proveit/logic/__init__.py
+++ proveit/logic/__init__.py
@@ -1,8 +1,10 @@
 from boolean import And, Or, Not, Implies, Iff, deriveStmtEqTrue, compose
-from boolean import TRUE, FALSE, BOOLEANS, inBool, deduceInBool
+from boolean import TRUE, FALSE, Booleans, inBool, deduceInBool
 from boolean import Forall, Exists, NotExists
 from set_theory import InSet, NotInSet, Singleton, Union, Intersect, Difference, SubsetEq, SupersetEq, SetOfAll
 from set_theory.common import NOTHING
 from equality import Equals, NotEquals, transitivitySearch
-from equality import autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
+from equality import SubstitutionMap, GlobalReplMap, SubExprMap
+#from equality import autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
+
 #from mapping.mappingOps import Domain, CoDomain
