24c88bb958948a1f65f44e73fc8fbdb36f8f8ad8
Wayne Witzel <wwitzel@sandia.gov>
2016-09-12 08:06:11 -0600

    Removed SubstitutionMap, etc.

--- proveit/logic/__init__.py
+++ proveit/logic/__init__.py
@@ -4,7 +4,6 @@ from boolean import Forall, Exists, NotExists
 from set_theory import InSet, NotInSet, Singleton, Union, Intersect, Difference, SubsetEq, SupersetEq, SetOfAll
 from set_theory.common import NOTHING
 from equality import Equals, NotEquals, transitivitySearch
-from equality import SubstitutionMap, GlobalReplMap, SubExprMap
 #from equality import autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
 
 #from mapping.mappingOps import Domain, CoDomain
