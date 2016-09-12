70c8486579e799d874221d0df917a4b364721dd0
Wayne Witzel <wwitzel@sandia.gov>
2016-09-12 08:05:06 -0600

    SubExprRepl -> SubExprMap consistently

--- proveit/lambda_map/sub_expr_repl.py
+++ proveit/lambda_map/sub_expr_repl.py
@@ -6,7 +6,7 @@ class SubExprRepl:
     Simply create a SubExprRepl with a master Expression and then access 
     sub-Expression attributes/items to obtain a map to the sub-Expression.
     For example, given an Expression `expr = (a + b + a/d)`, do
-    `SubExprMap(expr).operands[2].numerator` to obtain the SubExprMap
+    `SubExprRepl(expr).operands[2].numerator` to obtain the SubExprRepl
     that produces `_X_ -> (a + b + _X_/d)`.  This approach to generating
     the lambda map avoids ambiguity with respect to multiple
     sub-Expressions that are identical (in our example, the multiple `a`
@@ -15,11 +15,11 @@ class SubExprRepl:
     
     def __init__(self, masterExpr, subExprPath=tuple()):
         '''
-        Create an SubExprMap with the given master Expression.
+        Create an SubExprRepl with the given master Expression.
         Optionally, subExprPath is a sequence of sub-Expression indices
         starting from the master expression as the root to produce
         a map for replacing the corresponding sub-Expression.  A new
-        SubExprMap is generated appropriately, extending the 
+        SubExprRepl is generated appropriately, extending the 
         sub-Expression path, when getting an item or attribute
         that corresponds with an item/attribute of the currently
         mapped sub-Expression.
@@ -33,26 +33,26 @@ class SubExprRepl:
             
     def __getitem__(self, key):
         '''
-        If the currently mapped sub-Expression of the SubExprMap
+        If the currently mapped sub-Expression of the SubExprRepl
         is a Composite, accessing an item corresponding to an item
-        of this Composite will return the SubExprMap with the extended 
+        of this Composite will return the SubExprRepl with the extended 
         path to this item.
         '''
         curSubExpr = self.exprHierarchy[-1]
         if isinstance(curSubExpr, ExpressionList):
             # For an ExpressionList, the item key is simply the index of the sub-Expression
-            return SubExprMap(self.exprHierarchy[0], self.subExprPath + (key,))
+            return SubExprRepl(self.exprHierarchy[0], self.subExprPath + (key,))
         elif isinstance(curSubExpr, Composite):
             # For any other Composite (ExpressionTensor or NamedExpressions), the key is the key of the Composite dictionary.
             # The sub-Expressions are in the order that the keys are sorted.
             sortedKeys = sorted(curSubExpr.keys())
-            return SubExprMap(self.exprHierarchy[0], self.subExprPath + [sortedKeys.index(key)])
+            return SubExprRepl(self.exprHierarchy[0], self.subExprPath + [sortedKeys.index(key)])
         raise KeyError("The current sub-Expression is not a Composite.")
         
     def __getattr__(self, name):
         '''
         See if the attribute is accessing a sub-expression.  If so,
-        return the SubExprMap with the extended path to the sub-expression.
+        return the SubExprRepl with the extended path to the sub-expression.
         '''
         masterExpr = self.exprHierarchy[0]
         curSubExpr = self.exprHierarchy[-1]
@@ -66,7 +66,7 @@ class SubExprRepl:
         if isinstance(curSubExpr.__dict__[name], Expression):
             # The attribute is addressing an Expression of the current sub-Expression
             # and may be a sub-sub-Expression (or a member of sub-sub-Expression composite);
-            # if so, return the SubExprMap extended to the sub-sub-Expression.
+            # if so, return the SubExprRepl extended to the sub-sub-Expression.
             expr = curSubExpr.__dict__[name]
             for i, subSub in enumerate(subSubs):
                 if subSub == expr:
@@ -80,13 +80,13 @@ class SubExprRepl:
                         withDummyVars = curSubExpr.__class__.make(curSubExpr.coreInfo(), subSubs[:i] + [compositeWithDummyVars] + subSubs[i+1:])
                         if withDummyVars.__dict__[name] == compositeWithDummyVars:
                             # Checks out -- this is the right sub-sub-Expression, which is a Composite.
-                            return SubExprMap(self.exprHierarchy[0], self.subExprPath + (i,))                    
+                            return SubExprRepl(self.exprHierarchy[0], self.subExprPath + (i,))                    
                     else:
                         dummyVar = masterExpr.safeDummyVar()
                         withDummyVar = curSubExpr.__class__.make(curSubExpr.coreInfo(), subSubs[:i] + [dummyVar] + subSubs[i+1:])
                         if withDummyVar.__dict__[name] == dummyVars:
                             # Checks out -- this is the right sub-sub-Expression.
-                            return SubExprMap(self.exprHierarchy[0], self.subExprPath + (i,))
+                            return SubExprRepl(self.exprHierarchy[0], self.subExprPath + (i,))
                 elif isinstance(subSub, ExpressionList):
                     # An ExpressionList sub-Expression.  See if any of the elements are a match.
                     dummyVar = masterExpr.safeDummyVar()
@@ -96,7 +96,7 @@ class SubExprRepl:
                             listWithDummyVar = subSub[:j] + [dummyVar] + subSub[j+1:]
                             withDummyVar = curSubExpr.__class__.make(curSubExpr.coreInfo(), subSubs[:i] + [listWithDummyVar] + subSubs[i+1:])
                             if withDummyVar.__dict__[name] == dummyVar:
-                                return SubExprMap(self.exprHierarchy[0], self.subExprPath + (i, j))
+                                return SubExprRepl(self.exprHierarchy[0], self.subExprPath + (i, j))
                 elif isinstance(subSub, Composite):
                     # if the Composite isn't a list, then it is a dictionary (ExpressionTensor or NamedExpressions).
                     # See if any of the elements are a match.
@@ -107,12 +107,12 @@ class SubExprRepl:
                             dictWithDummyVar = {k:subSubSub if k==key else v for k,v in subSub.iteritems()}
                             withDummyVar = curSubExpr.__class__.make(curSubExpr.coreInfo(), subSubs[:i] + [dictWithDummyVar] + subSubs[i+1:])
                             if withDummyVar.__dict__[name] == dummyVar:
-                                return SubExprMap(self.exprHierarchy[0], self.subExprPath + (i, j))                            
+                                return SubExprRepl(self.exprHierarchy[0], self.subExprPath + (i, j))                            
             raise AttributeError("No attribute '%s' in '%s' and not a sub-Expression of '%s'"%(name, self.__class__, curSubExpr.__class__))
                     
     def lambdaMap(self):
         '''
-        Returns the lambda function/map that the SubExprMap is currently
+        Returns the lambda function/map that the SubExprRepl is currently
         producing.
         '''
         # build the lambda expression, starting with the lambda argument and
@@ -136,14 +136,14 @@ class SubExprRepl:
         Representation as NamedExpressions that indicates not only the lambda function
         but the sub-Expressions that may be accessed more deeply.
         '''
-        lambdaFn = self.lambdaFn()
-        lambdaArgs = lambdaFn.arguments
+        lambdaMap = self.lambdaMap()
+        lambdaArgs = lambdaMap.arguments
         curSubExpr = self.exprHierarchy[-1]
         if isinstance(curSubExpr, Composite):
             subExprs = list(curSubExpr.subExprIter())
         else:
             subExprs = [curSubExpr]
-        namedExprDict = {'lambda':lambdaFn}
+        namedExprDict = {'lambda':lambdaMap}
         namedExprDict.update({str(lambdaArg):subExpr for lambdaArg, subExpr in zip(lambdaArgs, subExprs)})
         return NamedExpressions(namedExprDict)        
     
