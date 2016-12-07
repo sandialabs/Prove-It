144340e015f3d64a5f68f59b62a4611f84e150ba
Wayne Witzel <wwitzel@sandia.gov>
2016-12-06 20:26:24 -0700

    changes to the way evaluation/reduction is implemented and related automation

--- proveit/logic/__init__.py
+++ proveit/logic/__init__.py
@@ -3,7 +3,8 @@ from boolean import TRUE, FALSE, Booleans, inBool, deduceInBool
 from boolean import Forall, Exists, NotExists
 from set_theory import InSet, NotInSet, Singleton, Union, Intersect, Difference, SubsetEq, SupersetEq, SetOfAll
 from set_theory.common import NOTHING
-from equality import Equals, NotEquals, transitivitySearch, defaultEvaluate, evaluateTruth, proveByEval
+from equality import Equals, NotEquals, transitivitySearch, IrreducibleValue
+from equality import reduceOperands, proveViaReduction, defaultEvaluate, evaluateTruth, EvaluationError
 #from equality import autoSubstitute, autoSubstitution, autoStatementSubstitution, generateSubExpressions, extractSubExpr
 
 #from mapping.mappingOps import Domain, CoDomain
