cde92143d85b9a979582bffea133a7f3be2e2d3e
Wayne Witzel <wwitzel@sandia.gov>
2017-05-20 08:44:15 -0600

    GreaterThan -> Greater, LessThan -> Less
    Missed this one from an earlier commit.

--- packages/proveit/number/ordering/__init__.py
+++ packages/proveit/number/ordering/__init__.py
@@ -1,5 +1,5 @@
-from greater_than import GreaterThan, GreaterThanEquals
-from less_than import LessThan, LessThanEquals
+from greater_than import Greater, GreaterEq
+from less_than import Less, LessEq
 from max import Max
 from min import Min
 
