ab22a49ca7ede71c600f39880d4c2e583809f151
Wayne Witzel <wwitzel@sandia.gov>
2019-02-04 10:51:44 -0700

    Removing _common_.py, _axioms_.py, and _theorems_.py which are autogenerated

--- packages/proveit/_common_.py
+++
@@ -1,3 +0,0 @@
-import sys
-from proveit._core_.context import CommonExpressions
-sys.modules[__name__] = CommonExpressions(__file__)
--- packages/proveit/logic/equality/_theorems_.py
+++
@@ -1,3 +0,0 @@
-import sys
-from proveit._core_.context import Theorems
-sys.modules[__name__] = Theorems(__file__)
