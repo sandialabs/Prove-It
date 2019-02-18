bf21abd3e5e4b793473e0e79592d1fb0212f35f8
Wayne Witzel <wwitzel@sandia.gov>
2019-02-18 10:39:37 -0700

    check python version

--- packages/proveit/__init__.py
+++ packages/proveit/__init__.py
@@ -1,3 +1,9 @@
+import sys
+if sys.version_info[0] < 3:
+    raise Exception("Must use Python 3")
+if sys.version_info[1] < 7:
+    raise Exception("Must use Python 3.7 or later")
+
 from ._core_ import defaults, USE_DEFAULTS, InvalidAssumptions, Context, ContextException
 from ._core_ import Expression, InnerExpr, Operation, OperationError, Function, OperationSequence, OperationOverInstances
 from ._core_ import Lambda, LambdaError, ParameterExtractionError, Label, Variable, Literal, DuplicateLiteralError
