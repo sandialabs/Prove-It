488d3246f4caf39669785ce3e6a20766b6e50bdc
Warren Craft <wdcraft@unm.ed>
2019-09-18 22:23:11 -0600

    Update Sqrt(2) Proof with Divides op update
    Implements a deduceInBool() method for the Divides class, provides
    the Latex format for the Divides operator, and utlizes the __file__ name
    for the context argument.

--- packages/proveit/number/divisibility/divides.py
+++ packages/proveit/number/divisibility/divides.py
@@ -1,9 +1,25 @@
-from proveit import Context, Operation, Literal
+from proveit import Context, Literal, Operation, USE_DEFAULTS
 
 class Divides(Operation):
     
-    context = Context('.')
-    _operator_ = Literal('|', context=context)
+    # context = Context('.')
+    # When I try to include latexFormat, I get an error
+    # 'Implicit operator cannot be changed'
+    _operator_ = Literal(
+    	  stringFormat='|', latexFormat=r'\rvert', context=__file__
+    	  )
+    # _operator_ = Literal('|', context=__file__)
     
     def __init__(self, a, b):
         Operation.__init__(self, Divides._operator_, [a, b])
+
+    def deduceInBool(self, assumptions=USE_DEFAULTS):
+        '''
+        Based on similar deduceInBool methods found in various classes in
+        logic.booleans. Attempt to deduce, then return, that this 'Divides'
+        expression is in the set of BOOLEANS.
+        '''
+        from ._axioms_ import dividesInBool
+        from proveit._common_ import m, n
+        return dividesInBool.specialize(
+        	  {m:self.operands[0], n:self.operands[1]}, assumptions=assumptions)
