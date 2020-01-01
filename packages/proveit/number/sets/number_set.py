9f051cde2544214fbd844562e990eb4ac3b56125
Wayne Witzel <wwitzel@sandia.gov>
2019-12-31 20:56:14 -0700

    Try simplifying before deducing an expression is in a number set.

--- packages/proveit/number/sets/number_set.py
+++ packages/proveit/number/sets/number_set.py
@@ -45,9 +45,26 @@ class NumberMembership(Membership):
         
     def conclude(self, assumptions=USE_DEFAULTS):
         '''
-        Try to deduce that the given element is in the number set under the given assumptions.
-        '''   
-        if hasattr(self.element, 'deduceInNumberSet'):
-            return self.element.deduceInNumberSet(self.number_set, assumptions=assumptions)
-        raise ProofFailure(InSet(self.element, self.number_set), assumptions, str(self.element) + " has no 'deduceInNumberSet' method.")
+        Try to deduce that the given element is in the number set under 
+        the given assumptions.
+        '''
+        element = self.element
+        # See if we can simplify the element first.
+        if hasattr(element, 'simplification'):
+            simplification = element.simplification(assumptions=assumptions)
+            element = simplification.rhs
+        if hasattr(element, 'deduceInNumberSet'):
+            elem_in_set = element.deduceInNumberSet(self.number_set, 
+                                                    assumptions=assumptions)
+            if elem_in_set is not None:
+                if element != self.element:
+                    # We have deduced that the simplified form is in the 
+                    # number set.  Substitute in the original.
+                    assert simplification is not None
+                    return simplification.subLeftSideInto(elem_in_set, assumptions)
+                else:
+                    return elem_in_set # This is what we want.
+        msg = str(self.element) + " has no 'deduceInNumberSet' method."
+        raise ProofFailure(InSet(self.element, self.number_set), 
+                            assumptions, msg)
 
