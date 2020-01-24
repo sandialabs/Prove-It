7d291ae6d0653cee0d70f5f25d7b88aeffe91f8e
Warren Craft <wdcraft@unm.ed>
2020-01-23 18:04:20 -0700

    Augment Set.deduceEnumSubsetEq() method with Set argument
    This will (eventually) allow users to provide an actual subset, in the form of an
    enumerated set like Set(a, b, c), as the proposed subset instead of just a list
    of indices to be used from the superset. This commit initially establishes this
    as an alternative method Set.deduceEnumSubsetEqAlt() for testing, allowing
    the subset argument and converting any subset to a list of indices. Still
    incomplete. Related issues: #77 , #69 , #62 .

--- packages/proveit/logic/set_theory/enumeration/enum_set.py
+++ packages/proveit/logic/set_theory/enumeration/enum_set.py
@@ -87,15 +87,120 @@ class Set(Operation):
         Deduce that this Set expression has as an improper subset the
         set specified by the indices in subset_indices list.
         For example,
-        {a, b, c, d}.deduceEnumSubset(subset_indices=[1, 3]) returns
+        {a, b, c, d}.deduceEnumSubsetEq(subset_indices=[1, 3]) returns
         |– {b, d} subsetEq {a, b, c, d}.
         This approach assumes we are not dealing with multisets
-        (or "bags").
+        (or "bags"), but often works with multisets anyway.
         '''
 
         from ._theorems_ import subsetEqOfSuperset
         from proveit._common_ import m, n, aa, bb
         from proveit.number import num
+
+        # check validity of provided subset_indices:
+        valid_indices_list = list(range(0, len(self.operands)))
+        self._check_subset_indices(valid_indices_list, subset_indices)
+
+        full_indices_list = list(range(0, len(self.operands)))
+
+        # construct the complement of the subset indices
+        # avoiding using sets to preserve order just in case
+        remaining_indices = list(full_indices_list) # clone
+        for elem in subset_indices:
+            remaining_indices.remove(elem)
+
+        new_order = subset_indices + remaining_indices
+        # find superset permutation needed for thm application
+        supersetPermRelation = generic_permutation(self, new_order, assumptions)
+        # construct the desired list of subset elems
+        desired_subset_list = []
+        for elem in subset_indices:
+            desired_subset_list.append(self.operands[elem])
+        # construct the desired complement list of elems
+        desired_complement_list = []
+        for elem in remaining_indices:
+            desired_complement_list.append(self.operands[elem])
+        # borrowed the following organization from apply_commutation_thm
+        m, n, a, b = subsetEqOfSuperset.allInstanceVars()
+        a_sub, b_sub = (desired_subset_list, desired_complement_list)
+        m_sub, n_sub = num(len(a_sub)), num(len(b_sub))
+        subset_of_permuted_superset = subsetEqOfSuperset.specialize(
+                {m:m_sub, n:n_sub, a:a_sub, b:b_sub},
+                assumptions=assumptions)
+
+        return supersetPermRelation.subLeftSideInto(subset_of_permuted_superset)
+
+    def deduceEnumSubsetEqAlt(self, subset_indices=None, subset=None,
+                         assumptions=USE_DEFAULTS):
+        '''
+        Deduce that this Set expression has as an improper subset the
+        set specified by either the indices in subset_indices list or
+        the Set() specified by subset.
+        For example, both
+        {a, b, c, d}.deduceEnumSubsetEq(subset_indices=[1, 3]) and
+        {a, b, c, d}.deduceEnumSubsetEq(subset=Set(b, d))
+        return |– {b, d} subsetEq {a, b, c, d}.
+        This approach assumes we are not dealing with multisets
+        (or "bags"). BUT it might still work with multisets!
+        This alt version is temporary to develop param input allowing
+        the specification of a subset itself rather than just indices.
+        '''
+
+        from ._theorems_ import subsetEqOfSuperset
+        from proveit._common_ import m, n, aa, bb
+        from proveit.number import num
+        from proveit.logic import Set
+
+
+        # check that user has specified subset_indices OR subset
+        # but NOT both
+        if subset_indices is None and subset is None:
+            raise ValueError("Need to specify the desired subset by "
+                             "specifying the list of indices (subset_indices) "
+                             "OR an actual subset (in the form of an "
+                             "enumerated set using Set()).")
+        if not subset_indices is None and not subset is None:
+            raise ValueError("Need to specify the desired subset by "
+                             "specifying the list of indices (subset_indices) "
+                             "OR an actual subset, but NOT both.")
+
+        # if subset has been specified, check that:
+        # (1) it is a valid Set()
+        # (2) has a valid cardinality
+        # (3) then convert to an list of indices
+        if subset is not None and not isinstance(subset, Set):
+            raise ValueError("Specified subset {} does not appear to be a "
+                             "valid Set object.".format(subset))
+        if (isinstance(subset, Set) and 
+            len(subset.operands) > len(self.operands)):
+            raise ValueError("Specified subset {0} with length {1} appears to "
+                             "be too large to be a subset of the superset "
+                             "{2} with length {3}.".
+                             format(subset, len(subset.operands),
+                                self.operands, len(self.operands)))
+
+        # if we make it this far and we have a subset provided
+        # convert to indices to make use of previously-established
+        # machinery
+        if subset is not None:
+            superset_list = list(self.operands)
+            superset_remaining_list = list(self.operands)
+            subset_list = list(subset.operands)
+            subset_indices_list = []
+            for elem in subset_list:
+                if elem in superset_remaining_list:
+                    superset_remaining_list.remove(elem)
+                    tempIdx = superset_list.index(elem)
+                    while tempIdx in subset_indices_list:
+                        tempIdx = superset_list.index(elem, tempIdx + 1)
+                    subset_indices_list.append(tempIdx)
+                else:
+                    raise ValueError("Element {0} of proposed subset does not "
+                                     "appear in superset {1} with sufficient "
+                                     "multiplicity.".
+                                     format(elem, superset_list))
+            print("subset_indices_list = {}".format(subset_indices_list))       # for testing; delete later
+
 
         # check validity of provided subset_indices:
         valid_indices_list = list(range(0, len(self.operands)))
