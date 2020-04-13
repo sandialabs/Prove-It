c1052db9d7951395e434b18728c8138e1ece7347
Warren Craft <wdcraft@unm.ed>
2020-04-13 11:25:15 -0600

    Remove _closureTheorm() method from Ceil, Floor, Round classes
    The _closureTheorem() method is obsolete. Related Issues: #97, #120.

--- packages/proveit/number/rounding/ceil.py
+++ packages/proveit/number/rounding/ceil.py
@@ -13,13 +13,6 @@ class Ceil(Function):
         # self.operand = A # check later that the operand attribute
         # is still working!
 
-    def _closureTheorem(self, numberSet):
-        from . import _theorems_
-        if numberSet == NaturalsPos:
-            return _theorems_.ceilRealPosClosure
-        elif numberSet == Integers:
-            return _theorems_.ceilRealClosure
-
     def latex(self, **kwargs):
         return r'\lceil ' + self.operand.latex(fence=False) + r'\rceil'
 
@@ -80,70 +73,6 @@ class Ceil(Function):
         from ._theorems_ import ceilOfInteger
 
         return apply_roundingElimination(self, ceilOfInteger, assumptions)
-
-    # def roundingExtraction(self, idx_to_extract=None, assumptions=USE_DEFAULTS):
-    #     '''
-    #     For the case of Ceil(x) where the operand x = x_real + x_int,
-    #     derive and return Ceil(x) = Ceil(x_real) + int (thus
-    #     'extracting' the integer component out from the Ceil() fxn).
-    #     The idx_to_extract is the zero-based index of the item in the
-    #     operands of an Add(a, b, …, n) expression to attempt to extract.
-    #     Assumptions may be necessary to deduce necessary conditions
-    #     (for example, that x_int actually is an integer) for the
-    #     simplification.
-    #     This method is utilized by the doReducedSimplification() method
-    #     only after the operand x is verified to already be proven
-    #     (or assumed) to be of the form x = x_real + x_int.
-    #     For the case where the entire operand x is itself an integer,
-    #     see the roundingElimination() method.
-
-    #     This works only if the operand x is an instance of the Add
-    #     class at its outermost level, e.g. x = Add(a, b, …, n). The
-    #     operands of that Add class can be other things, but the
-    #     extraction is guaranteed to work only if the inner operands
-    #     a, b, etc., are simple.
-    #     '''
-    #     from proveit import TransRelUpdater
-    #     from proveit._common_ import n, x, y
-    #     from proveit.logic import Equals
-    #     from proveit.number import one, two, Add
-    #     from ._theorems_ import ceilOfRealPlusInt
-
-    #     # among other things, convert any assumptions=None
-    #     # to assumptions=() to avoid later len() errors
-    #     assumptions = defaults.checkedAssumptions(assumptions)
-
-    #     expr = self
-
-    #     # for convenience while updating our equation
-    #     eq = TransRelUpdater(expr, assumptions)
-
-    #     # first use Add.commutation to (re-)arrange operands to comform
-    #     # to theorem format, using user-supplied idx_to_extract
-    #     if isinstance(self.operand, Add):
-    #         expr = eq.update(self.innerExpr().operand.commutation(idx_to_extract,
-    #                 len(self.operand.operands)-1, assumptions=assumptions))
-
-    #         # An association step -- because the later application of
-    #         # the floorOfRealPlusInt thm produces a grouping of the 
-    #         # Floor operands in the chain of equivalences.
-    #         # BUT, only perform the association if multiple operands are
-    #         # needing to be associated:
-    #         if len(expr.operand.operands)-1 > 1:
-    #             expr = eq.update(expr.innerExpr().operand.association(
-    #                     0, len(expr.operand.operands)-1, assumptions=assumptions))
-
-
-    #         # then update by applying the floorOfRealPlusInt thm
-    #         x_sub = expr.operand.operands[0]
-    #         n_sub = expr.operand.operands[1]
-    #         expr = eq.update(ceilOfRealPlusInt.specialize(
-    #                 {x:x_sub, n:n_sub}, assumptions=assumptions))
-
-    #         return eq.relation
-    #     else:
-    #         raise ValueError("In attempting Ceil(x).roundingExtraction(), "
-    #                          "the operand x is not of class 'Add'.")
 
     def roundingExtraction(self, idx_to_extract=None, assumptions=USE_DEFAULTS):
         '''
