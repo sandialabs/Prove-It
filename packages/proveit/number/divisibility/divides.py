5230c4896c9cae7a3ac2fab5bcfea3e540eabec0
Warren Craft <wdcraft@unm.edu>
2020-08-28 12:11:17 -0600

    Add DividesRelation class and change Divides to extend DividesRelation
    Add a DividesRelation class extending TransitiveRelation and change Divides class to extend DividesRelation. Also add a DividesProper class. All such changes combined then then allow call Divides.concludeViaTransitivity() which accesses the TransitiveRelation.concludeViaTransitivity() method. Related Issues: #179, #93.

--- packages/proveit/number/divisibility/divides.py
+++ packages/proveit/number/divisibility/divides.py
@@ -1,13 +1,45 @@
-from proveit import (Context, defaults, Literal, Operation,
+from proveit import (asExpression, Context, defaults, Literal, Operation,
                      ProofFailure, TransitiveRelation, USE_DEFAULTS)
 
-class Divides(TransitiveRelation):
+class DividesRelation(TransitiveRelation):
+
+    def __init__(self, operator, lhs, rhs):
+        TransitiveRelation.__init__(self, operator, lhs, rhs)
+
+    def lower(self):
+        '''
+        Returns the lower bound side of this inequality.
+        '''
+        return self.lhs
+
+    def upper(self):
+        '''
+        Returns the upper bound side of this inequality.
+        '''
+        return self.rhs
+    
+    @staticmethod
+    def WeakRelationClass():
+        # Divides is weaker than DividesProper (yet to be defined)
+        # analogous to <= being weaker than <
+        return Divides # Divides is weaker than DividesProper
+
+    @staticmethod
+    def StrongRelationClass():
+        # might later have a DividesProper here, which would
+        # be like Divides(x, y) but requiring x != y
+        return DividesProper # DividesProper is stronger than Divides               
+
+
+class Divides(DividesRelation):
     '''
     Divides(x, y) represents the claim that x divides y (i.e. x|y)
     (equivalently, that x is a factor of y, y is a multiple of x,
     or y/x is an integer). The underlying axiomatic definition uses
-    the y/x is an integer formulation and allows x and y to be any
-    complex number as long as x ≠ 0.
+    x|y = (y/x in Z), with no prerequisites on x and y. Extending
+    DividesRelation allows the possibility (later) of having not only
+    this weak relation class but also a strong relation class, perhaps
+    DividesProper, yet to be defined.
     '''
 
     _operator_ = Literal(
@@ -22,13 +54,13 @@ class Divides(TransitiveRelation):
     knownRightSides = dict() 
     
     def __init__(self, a, b):
-        TransitiveRelation.__init__(self, Divides._operator_, a, b)
+        DividesRelation.__init__(self, Divides._operator_, a, b)
 
     def conclude(self, assumptions=USE_DEFAULTS):
         '''
         Attempt to conclude the divisibility claim in various ways:
-        (1) simple reflexivity (x=x);
-        (2) simple 0/x for x ≠ 0;
+        (1) simple reflexivity (x|x);
+        (2) simple x|0 for x ≠ 0;
         (3) via transitivity.
         '''
         # among other things, convert any assumptions=None
@@ -67,6 +99,21 @@ class Divides(TransitiveRelation):
                 # We have 0/x with complex x ≠ 0
                 return self.concludeViaZeroFactor(assumptions)
 
+        #-- -------------------------------------------------------- --#
+        #-- Case (3): x|z with x|y and y|z known or assumed          --#
+        #-- -------------------------------------------------------- --#
+        # seek out the appropriate x|y and y|z and use transitivity
+        # to get x|z
+        # try:
+        #     return self.concludeViaTransitivity(assumptions)
+        # except:
+        #     pass
+        return self.concludeViaTransitivity(assumptions)
+
+        raise ProofFailure(self, assumptions, 
+                           "Unable to automatically conclude by "
+                           "standard means.")
+
 
     def concludeViaReflexivity(self, assumptions=USE_DEFAULTS):
         '''
@@ -87,12 +134,70 @@ class Divides(TransitiveRelation):
        return nonZeroDividesZero.instantiate(
                 {_x:self.lhs}, assumptions=assumptions)
 
+    def applyTransitivity(self, other, assumptions=USE_DEFAULTS):
+        '''
+        From x|y (self) and y|z (other) derive and return x|z.
+        Will also check for y|z (self) and x|y (other) to return x|z.
+        '''
+        from ._theorems_ import dividesTransitivity
+        _x, _y, _z = dividesTransitivity.instanceParams
+        other = asExpression(other)
+        if not isinstance(other, Divides):
+            raise ValueError(
+                "Unsupported transitivity operand: Divides.applyTransitivity() "
+                "method requires another Divides expression for transitivity "
+                "to be applied. Instead, received the following input: {0}.".
+                format(other))
+
+        if self.rhs == other.lhs:
+            return dividesTransitivity.instantiate(
+                    {_x:self.lhs, _y:self.rhs, _z:other.rhs},
+                    assumptions=assumptions)
+        if self.lhs == other.rhs:
+            return dividesTransitivity.instantiate(
+                    {_x:other.lhs, _y:other.rhs, _z:self.rhs},
+                    assumptions=assumptions)
+        else:
+            raise TransitivityException(
+                self, assumptions,
+                'Transitivity cannot be applied unless the lhs of one of '
+                'Divides expressions is equal to the rhs of the other '
+                'Divides expression. Instead we have: {0} vs {1}'.
+                format(self, other))
+
     def deduceInBool(self, assumptions=USE_DEFAULTS):
         '''
         Deduce and return that this 'Divides' expression is in
         the set of Booleans.
         '''
         from ._theorems_ import dividesInBool
-        x, y = dividesInBool.instanceVars
+        _x, _y = dividesInBool.instanceParams
         return dividesInBool.specialize(
-        	  {x:self.operands[0], y:self.operands[1]}, assumptions=assumptions)
+        	  {_x:self.operands[0], _y:self.operands[1]},
+              assumptions=assumptions)
+
+class DividesProper(DividesRelation):
+    '''
+    DividesProper(x, y) represents the claim that x divides y
+    (i.e. x|y) but x != y. x|y is equivalent to saying that x is a
+    factor of y, y is a multiple of x, or y/x is an integer).
+    This class is still under development, with the class stub located
+    here so that we have a 'strong relation' class version of Divides
+    that can be called from the DividesRelation class, which in turn
+    allows us to use the TransitiveRelation.concudeViaTransitivity()
+    method.
+    '''
+
+    _operator_ = Literal(
+          stringFormat='|', latexFormat=r'\rvert_{P}', context=__file__
+          )
+
+    # map left-hand-sides to "Divides" KnownTruths
+    #   (populated in TransitivityRelation.sideEffects)
+    knownLeftSides = dict()    
+    # map right-hand-sides to "Divides" KnownTruths
+    #   (populated in TransitivityRelation.sideEffects)
+    knownRightSides = dict() 
+    
+    def __init__(self, a, b):
+        DividesRelation.__init__(self, DividesProper._operator_, a, b)
