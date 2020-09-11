e944b12409c62cb25283330cc7f9d900ecc1d574
Warren Craft <wdcraft@unm.edu>
2020-09-11 16:39:27 -0600

    Add GCD.deduce_relatively_prime() method in divisibility package.
    Related issue #93. Add GCD.deduce_relatively_prime() method in divisibility package. This method instantiates the GCD_one_def thm in GCD pacakge that defines what it means for two elements to have GCD = 1.

--- packages/proveit/number/divisibility/greatest_common_divisor.py
+++ packages/proveit/number/divisibility/greatest_common_divisor.py
@@ -16,13 +16,8 @@ class GCD(Function):
         the universally-quantified claim that for all natural positive
         numbers p > 1 we have Not(p|a AND p|b).
         '''
-        if Equals(self, one).proven(assumptions):
-            from ._theorems_ import GCD_one_def
-            _a, _b = GCD_one_def.instanceParams
-            return GCD_one_def.instantiate(
-            	{_a:self.operands[0], _b:self.operands[1]},
-            	assumptions=assumptions)
-        else:
-            raise ValueError(
-            	      "In GCD.deduce_relatively_prime(), {0}=1 is not known "
-            	      "or assumed to be true.".format(self))
+        from ._theorems_ import GCD_one_def
+        _a, _b = GCD_one_def.instanceParams
+        return GCD_one_def.instantiate(
+        	{_a:self.operands[0], _b:self.operands[1]},
+        	assumptions=assumptions)
