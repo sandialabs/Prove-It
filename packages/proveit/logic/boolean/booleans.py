02a5a6428402f0c2665ef7b45d1f35de0885ad34
wwitzel <wwitzel@sandia.gov>
2020-09-22 10:23:39 -0600

    Added FalseLiteral.denyAssumption
    as a convenience for contradiction proofs.

--- packages/proveit/logic/boolean/booleans.py
+++ packages/proveit/logic/boolean/booleans.py
@@ -270,6 +270,15 @@ class FalseLiteral(Literal, IrreducibleValue):
     def deduceInBool(self, assumptions=USE_DEFAULTS):
         from ._theorems_ import falseInBool
         return falseInBool
+    
+    def denyAssumption(self, assumption_to_deny, assumptions=USE_DEFAULTS):
+        '''
+        If FALSE can be proven under a set of assumptions, any one
+        of those assumptions may be proven untrue given the other
+        assumptions.
+        '''
+        impl = self.prove(assumptions).asImplication(assumption_to_deny)
+        return impl.denyAntecedent()
 
 def inBool(*elements):
     from proveit.logic.set_theory import InSet
