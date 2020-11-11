eac42c8676c7b6f94bb39695d6df0466c875f7a4
Joaquin <joaquinemilio7@gmail.com>
2020-11-11 09:39:59 -0700

    Fixing broken notebooks a result of the naturals move
    Fixing try/except block in enum_membership.py

--- packages/proveit/logic/set_theory/enumeration/enum_membership.py
+++ packages/proveit/logic/set_theory/enumeration/enum_membership.py
@@ -26,20 +26,23 @@ class EnumMembership(Membership):
         From [(element=x) or (element=y) or ..], derive and return
         [element in {x, y, ..}].
         '''
-        from ._theorems_ import foldSingleton, inEnumeratedSet
+        from proveit import ProofFailure
+        from ._theorems_ import foldSingleton, inEnumeratedSet, fold
         enum_elements = self.domain.elements
         if len(enum_elements) == 1:
             return foldSingleton.instantiate(
                 {x:self.element, y:enum_elements[0]}, assumptions=assumptions)
         else:
-            idx = self.domain.operands.index(self.element)
-            _a = self.domain.operands[:idx]
-            _b = self.element
-            _c = self.domain.operands[idx + 1:]
-            _m = ExprTuple(*_a).length(assumptions=assumptions)
-            _n = ExprTuple(*_c).length(assumptions=assumptions)
-            return inEnumeratedSet.instantiate({m:_m, n:_n, a:_a, b:_b, c:_c}, assumptions=assumptions)
-            #return fold.instantiate({n:num(len(enum_elements)), x:self.element, y:enum_elements}, assumptions=assumptions)
+            try:
+                idx = self.domain.operands.index(self.element)
+                _a = self.domain.operands[:idx]
+                _b = self.element
+                _c = self.domain.operands[idx + 1:]
+                _m = ExprTuple(*_a).length(assumptions=assumptions)
+                _n = ExprTuple(*_c).length(assumptions=assumptions)
+                return inEnumeratedSet.instantiate({m:_m, n:_n, a:_a, b:_b, c:_c}, assumptions=assumptions)
+            except (ProofFailure, ValueError) as e:
+                return fold.instantiate({n:num(len(enum_elements)), x:self.element, y:enum_elements}, assumptions=assumptions)
 
     def equivalence(self, assumptions=USE_DEFAULTS):
         '''
