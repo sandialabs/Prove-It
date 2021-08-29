d48533cf4c6fc3d550c02fcd10b34412f81e6668
wwitzel <wwitzel@sandia.gov>
2021-08-28 22:10:47 -0600

    Updates in linear_algebra package: organization and linear combination closure
    Moving theorems into sub-packages as appropriate.  Adding vector addition and linear combination closure theorems and related automation.

--- packages/proveit/linear_algebra/scalar_multiplication/scalar_mult.py
+++ packages/proveit/linear_algebra/scalar_multiplication/scalar_mult.py
@@ -1,4 +1,8 @@
-from proveit import Operation, Literal
+from proveit import (Operation, Literal, relation_prover,
+                     UnsatisfiedPrerequisites)
+from proveit import a, x, K, V
+from proveit.logic import InSet
+from proveit.linear_algebra import VecSpaces
 
 class ScalarMult(Operation):
     '''
@@ -10,7 +14,7 @@ class ScalarMult(Operation):
     # the literal operator of the MatrixProd operation
     # perhaps use SCALAR_PROD for string?
     # latex_format try using \; in place of a blank space
-    _operator_ = Literal(string_format=r'*', latex_format=r'\thinspace',
+    _operator_ = Literal(string_format=r'*', latex_format=r'\cdot',
                          theory=__file__)
 
     def __init__(self, scalar, scaled, *, styles=None):
@@ -21,3 +25,28 @@ class ScalarMult(Operation):
                            styles=styles)
         self.scalar = scalar
         self.scaled = scaled
+
+    @relation_prover
+    def deduce_in_vec_space(self, vec_space=None, *, field,
+                            **defaults_config):
+        '''
+        Prove that this scaled vector is in a vector space.  The vector
+        space may be specified or inferred via known memberships.  
+        A field for the vector space must be specified.
+        '''
+        from . import scalar_mult_closure
+        if vec_space is None:
+            # No vector space given, so we'll have to look for
+            # a known membership of 'scaled' in a vector space.
+            # This may be arbitrarily chosen.
+            try:
+                vec_space = next(VecSpaces.yield_known_vec_spaces(
+                        self.scaled, field=field))
+            except StopIteration:
+                # No known vector space membership over the specified
+                # field.
+                raise UnsatisfiedPrerequisites(
+                        "%s is not known to be a vector in a vector "
+                        "space over %s"%(self.scaled, field))
+        return scalar_mult_closure.instantiate(
+                {K:field, V:vec_space, a:self.scalar, x:self.scaled})
--- packages/proveit/linear_algebra/vector_spaces.py
+++ packages/proveit/linear_algebra/vector_spaces.py
@@ -1,4 +1,5 @@
 from proveit import Operation, Function, Literal
+from proveit.logic import InClass, ClassMembership, InSet
 
 class VecSpaces(Function):
     '''
@@ -9,14 +10,27 @@ class VecSpaces(Function):
     in order to contain any members (or even define membership).
     This will allow us to use VecSpaces without an explicit constraint
     on its 'field' operand.
+    
+    Expression types that may represent vector spaces may implement a
+    'deduce_as_vec_space' method to prove its membership in
+    the appropriate class of vector spaces over a provided 'field'.
     '''
     
     _operator_ = Literal(
             string_format=r'VecSpaces', latex_format=r'\textrm{VecSpaces}',
             theory=__file__)
     
+    # Map vector spaces to their known membership(s) within 
+    # VecSpaces(K) for some field K. Such a membership relation is the 
+    # indication that it is a vector space over the corresponding field.
+    known_vec_spaces_memberships = dict() 
+    
     def __init__(self, field, *, styles=None):
         Function.__init__(self, VecSpaces._operator_, field, styles=styles)
+        self.field = field
+    
+    def membership_object(self, element):
+        return VecSpacesMembership(element, self)
     
     @property
     def is_proper_class(self):
@@ -25,3 +39,56 @@ class VecSpaces(Function):
         InClass should be used instead of InSet when this is a domain.
         '''
         return True
+
+    @classmethod
+    def yield_known_vec_spaces(self, vec, *, field):
+        '''
+        Given a vector expression, vec, yield any vector spaces,
+        over the specified field, known to contain vec.
+        '''
+        for membership in InSet.known_memberships[vec]:
+            # Check if the membership domain is a vector space over the
+            # specified field.
+            is_known_vec_space = InClass(membership.domain, 
+                                         VecSpaces(field)).proven()
+            if not is_known_vec_space:
+                if hasattr(membership.domain, 'deduce_as_vec_space'):
+                    # See if we can prove that the domain is a vector
+                    # space via 'deduce_as_vec_space'.
+                    try:
+                        membership.domain.deduce_as_vec_space(field=field)
+                        # Assume it worked.
+                        is_known_vec_space = True
+                    except NotImplementedError:
+                        pass
+            if is_known_vec_space:
+                # Match found: vec is a member of a domain
+                # that is a vector space over the specified field.
+                yield membership.domain
+
+class VecSpacesMembership(ClassMembership):
+    def __init__(self, element, domain):
+        ClassMembership.__init__(self, element, domain)
+        if not isinstance(domain, VecSpaces):
+            raise TypeError("domain expected to be VecSpaces, not %s"
+                            %domain.__class__)
+        self.field = domain.field
+    
+    def side_effects(self, judgment):
+        '''
+        Remember known VecSpaces memberships.
+        '''
+        VecSpaces.known_vec_spaces_memberships.setdefault(
+                self.element, set()).add(judgment)
+    
+    def conclude(self):
+        '''
+        Attempt to conclude this membership in a class of vector
+        spaces.
+        '''
+        if hasattr(self.element, 'deduce_as_vec_space'):
+            return self.element.deduce_as_vec_space(field=self.field)
+        raise NotImplementedError(
+                "VecSpacesMembership.conclude is only implemented when "
+                "the element has a 'deduce_as_vec_space' method; %s "
+                "dows not have such a method"%self.element.__class__)
