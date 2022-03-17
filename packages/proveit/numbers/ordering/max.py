77b16f1c7e09d447c777c1f43f57e553e4598d34
Warren Craft <wdcraft@unm.edu>
2022-03-17 17:36:28 -0600

    Update Max.shallow_simplification()
    Related Issues: #97, #275, #281. Update Max.shallow_simplification() method to deal with multi-argument version and deal with cases requiring commuting of binary arguments.

--- packages/proveit/numbers/ordering/max.py
+++ packages/proveit/numbers/ordering/max.py
@@ -1,8 +1,8 @@
-from proveit import (equality_prover, Literal, Function, relation_prover,
-                      UnsatisfiedPrerequisites)
+from proveit import (equality_prover, Literal, ExprRange, Function,
+                     relation_prover, UnsatisfiedPrerequisites)
 from proveit.numbers import (
         deduce_number_set, merge_list_of_sets, NumberOperation, Real)
-from proveit import a, n, S
+from proveit import a, b, m, n, x, y, S
 
 
 class Max(NumberOperation, Function):
@@ -15,34 +15,106 @@ class Max(NumberOperation, Function):
     def __init__(self, *operands, styles=None):
         NumberOperation.__init__(self, Max._operator_, operands, styles=styles)
 
+    @equality_prover('defined', 'define')
+    def definition(self, **defaults_config):
+        '''
+        For the unary case, where self = Max(a), deduce and return
+
+            [Max(a) = a].
+
+        For the binary case, where self = Max(a, b), deduce and return 
+
+            [Max(a, b)] = [ a if a >= b,
+                            b if b >  a ].
+
+        For the more general case (without an ExprRange), deduce and
+        return
+
+            [Max(a_1, a_2, ..., a_m, b)] =
+             Max(Max(a_1, a_2, ..., a_{m}), b)
+
+        For the binary case, if the conditions in the conditional fxn
+        definition are known, then the Max.shallow_simplification()
+        method may automatically further evaluate or simplify the rhs.
+        If such further reduction is not desired, include
+        auto_simplify=False as an argument.
+        For the general case with an ExprRange in the arguments, the
+        results are unpredictable.
+        '''
+        if self.operands.is_single():
+            from . import max_def_unary
+            _x_sub = self.operand
+            return max_def_unary.instantiate({x: _x_sub})
+        elif self.operands.is_double():
+            from . import max_def_bin
+            _x_sub = self.operands[0]
+            _y_sub = self.operands[1]
+            return max_def_bin.instantiate({x: _x_sub, y: _y_sub})
+        else:
+            from . import max_def_multi
+            _a_sub = self.operands[0:-1]
+            _b_sub = self.operands[-1]
+            _m_sub = _a_sub.num_elements()
+            return max_def_multi.instantiate(
+                    {m: _m_sub, a: _a_sub, b: _b_sub})
+
     @equality_prover('shallow_simplified', 'shallow_simplify')
     def shallow_simplification(self, *, must_evaluate=False, **defaults_config):
         '''
+        Returns a proven simplification equation for this Max
+        expression assuming the operand(s) has/have been simplified
+        and assuming the operand(s) is/are Real.
+        For the simple unary case Max(a), returns the proven
+        simplification Max(a) = a.
         For the simple binary case Max(a, b), returns a proven
         simplification equation for this Max expression assuming the
         operands 'a' and 'b' have been simplified and that we know or
-        have assumed that either a >= b or b > a. If such relational
-        knowledge is not to be had, we simply return the equation of
-        the Max expression with itself.
-        Cases with more than 2 operands are not yet handled.
+        have assumed that either a >= b or b > a.
+        For the more general case Max(a_1, a_2, ..., a_m, b), returns a
+        proven simplification equation of the form
+        self = Max(Max(a_1, a_2, ..., a_m), b), where the nested Max()
+        call might then be further simplified.
+        For the binary case, if the requisite relational knowledge
+        among arguments is not to be had, we simply return the equation
+        of the Max expression with itself.
+        Cases involving ExprRanges are not currently handled, instead
+        simply returning the proven judgment |- self=self.
         '''
         from proveit.logic import Equals
         from proveit.numbers import greater_eq
 
-        # We're only set up to deal with binary operator version
-        if not self.operands.is_double():
-            # Default is no simplification if not a binary operation.
-            return Equals(self, self).prove()
+        if self.operands.is_single():
+            from . import max_def_unary
+            _x_sub = self.operand
+            return max_def_unary.instantiate({x: _x_sub})
 
         # If binary and we know how operands 'a' and 'b' are related ...
-        op_01, op_02 = self.operands[0], self.operands[1]
-        if (greater_eq(op_01, op_02).proven()
-            or greater_eq(op_02, op_01).proven()):
-            from proveit import x, y
+        elif self.operands.is_double():
             from proveit.numbers.ordering import max_def_bin
-            return max_def_bin.instantiate({x: op_01, y: op_02})
+            op_01, op_02 = self.operands[0], self.operands[1]
+            if (greater_eq(op_01, op_02).proven()):
+                return max_def_bin.instantiate({x: op_01, y: op_02})
+            elif (greater_eq(op_02, op_01).proven()):
+                from proveit import TransRelUpdater
+                from proveit.numbers.ordering import max_bin_args_commute
+                expr = self
+                eq = TransRelUpdater(expr) # begins as self = self
+                expr = eq.update(max_bin_args_commute.instantiate(
+                        {x: op_01, y: op_02}, preserve_all=True))
+                # then simplify the rhs only
+                expr = eq.update(expr.simplification())
+                return eq.relation
 
-        # Otherwise still no simplification.
+        # If multi-argument but no ExprRanges
+        elif (not any([isinstance (op, ExprRange) for op in self.operands])):
+            from . import max_def_multi
+            _a_sub = self.operands[0:-1]
+            _b_sub = self.operands[-1]
+            _m_sub = _a_sub.num_elements()
+            return max_def_multi.instantiate(
+                    {m: _m_sub, a: _a_sub, b: _b_sub})
+
+        # Otherwise no simplification.
         return Equals(self, self).prove()
 
     @relation_prover
