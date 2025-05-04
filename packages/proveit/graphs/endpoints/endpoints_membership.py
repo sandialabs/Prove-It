5dcc9158e04cccb519b1c95867941f3dc9642815
Warren Craft <wdcraft@unm.edu>
2025-05-04 07:47:20 -0500

    Update EndpointsMembership methods
    Update EndpointsMembership methods with introduction of EndpointsMembership.definition() method.

--- packages/proveit/graphs/endpoints/endpoints_membership.py
+++ packages/proveit/graphs/endpoints/endpoints_membership.py
@@ -10,6 +10,21 @@ class EndpointsMembership(SetMembership):
     def __init__(self, element, domain):
         SetMembership.__init__(self, element, domain)
 
+    @equality_prover('defined', 'define')
+    def definition(self, **defaults_config):
+        '''
+        From self = [elem in Endpoints(P)], where P is a non-trivial
+        path (i.e. a path with at least one edge), deduce and return:
+            [elem in Vertices(Graph(V,E))] = [elem in V]
+        '''
+
+        from . import membership_def
+        element = self.element
+        _V_sub  = self.domain.graph.vertex_set
+        _E_sub  = self.domain.graph.edge_set
+        return membership_def.instantiate(
+                {v:element, V:_V_sub, E:_E_sub },auto_simplify=False)
+
 
 class EndpointsNonmembership(SetNonmembership):
     '''
