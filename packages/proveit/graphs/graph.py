73703ed1b2d939071c7b135c752a081e00923c24
Warren Craft <wdcraft@unm.edu>
2025-05-04 07:44:40 -0500

    Update graphs.py, shfiting Path class to paths pkg
    Update graphs.py, deleting the Path class which now appears in graphs/paths theory package inside paths.py.

--- packages/proveit/graphs/graph.py
+++ packages/proveit/graphs/graph.py
@@ -66,31 +66,4 @@ class Graph(Function):
                 self, Graph._operator_, (V, E), styles=styles)
 
 
-class Path(Graph):
-    '''
-    Path(V,E) represents a path. A Path is a non-empty graph
-    P = (V,E) with vertex set V and edge set E such that:
 
-        V = {x0, x1, ..., xk}, E = {x0x1, x1x2, ..., x_{k-1}x_{k}},
-
-    where the x_{i} are all distinct. The vertices x0 and xk are said
-    to be linked by the path P and are called its endvertices, or
-    endpoints, or simply its ends. The vertices x1, ..., x_{k-1} are
-    the inner vertices of P. The number of edges in the path P,
-    denoted ||P||, is its length (in this example, ||P|| = k).
-    '''
-
-    # the literal operator of the Path operator
-    _operator_ = Literal(string_format='Path',
-                         latex_format=r'\text{Path}',
-                         theory=__file__)
-
-    def __init__(self, V, E, *, styles=None):
-        '''
-        Create or represent a Path, Path(V,E), with vertex set V
-        and edge set E.
-        '''
-        self.vertex_set = V
-        self.edge_set   = E
-        Function.__init__(
-                self, Path._operator_, (V, E), styles=styles)
