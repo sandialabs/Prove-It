54f3781826b53a8de9e59f87c9140ebd002c58cb
Warren Craft <wdcraft@unm.edu>
2025-06-04 15:50:33 -0500

    Establish IntegerOddSet number set class
    Establish the IntegerOddSet number set class to represent the odd integers. Also assumes the establishment of the IntegerOddMembership class, in a separate (later) commit.

--- packages/proveit/numbers/number_sets/integers/integer.py
+++ packages/proveit/numbers/number_sets/integers/integer.py
@@ -288,6 +288,21 @@ class IntegerEvenSet(NumberSet):
         from .integer_membership import IntegerEvenMembership    
         return IntegerEvenMembership(element)
 
+class IntegerOddSet(NumberSet):
+    '''
+    Class to represent the odd integers {..., -3, -1, 1, 3, ...}
+    and provide related methods. The odd integer set appears as
+    'IntegerOdd' in string output and as mathbb 'O' in LaTeX output.
+    '''
+    def __init__(self, *, styles=None):
+        NumberSet.__init__(self, 'IntegerOdd', r'\mathbb{O}',
+                           theory=__file__, styles=styles,
+                           fence_when_forced=True)
+
+    def membership_object(self, element):
+        from .integer_membership import IntegerOddMembership    
+        return IntegerOddMembership(element)
+
 
 if proveit.defaults.running_theory_notebook is None:
     # Import some fundamental theorems without quantifiers when not 
