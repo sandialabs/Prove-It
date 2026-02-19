7b7241cf4f5d286f208dc4b701da0a56027ee8b8
wwitzel <wwitzel@sandia.gov>
2026-02-18 17:51:05 -0700

    decorators.py: set defaults.preserved_exprs for is_simplification_method

--- packages/proveit/decorators.py
+++ packages/proveit/decorators.py
@@ -223,14 +223,17 @@ def _make_decorated_prover(func):
                         internal_kwargs['markers_and_marked_expr'] = (
                             markers_and_marked_expr)
                     '''
-                    temp_defaults.preserve_all=True
+                    
                     temp_defaults.preserved_exprs = set()
                     if is_simplification_method:
-                        internal_kwargs['preserved_exprs'] = preserved_exprs
+                        temp_defaults.preserved_exprs = preserved_exprs
                         internal_kwargs['simplify_only_where_marked'] = (
                             simplify_only_where_marked)
                         internal_kwargs['markers_and_marked_expr'] = (
                             markers_and_marked_expr)
+                    else:
+                        temp_defaults.preserve_all=True
+                        temp_defaults.preserved_exprs = set()
                     proven_truth = checked_truth(func(*args, **internal_kwargs))
 
         else:
