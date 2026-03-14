0c00a1d15a5e4e6461ae78d7d5dc090d7a011a2e
TushtiPatel <132899216+TushtiPatel@users.noreply.github.com>
2026-03-14 17:31:47 +0530

    dist_between_two_vecs_theorem_proof

--- packages/proveit/linear_algebra/inner_products/inner_prod.py
+++ packages/proveit/linear_algebra/inner_products/inner_prod.py
@@ -1,7 +1,9 @@
 from proveit import (Operation, Literal, UnsatisfiedPrerequisites,
                      equality_prover)
-from proveit import a, b, x, y, z, H, K
-from proveit.logic import is_irreducible_value
+from proveit import a, b, x, y, z, u, H, K
+from proveit.logic import is_irreducible_value, Equals
+from proveit.numbers import one 
+
 
 class InnerProd(Operation):
     _operator_ = Literal(
@@ -11,15 +13,82 @@ class InnerProd(Operation):
     def __init__(self, a, b, *, styles=None):
         Operation.__init__(self, InnerProd._operator_,
                            (a, b), styles=styles)
+
+    def style_options(self):
+        options = super().style_options()
+        options.add_option(
+            name='notation',
+            description="Inner product notation: 'standard' or 'dirac'",
+            default='standard',
+            related_methods=()
+        )
+        return options
+
     
-    def string(self, **kwargs):
-        _a, _b = self.operands
-        return '<' + _a.string() + ', ' + _b.string() + '>'
-    
+
+    def with_notation(self, notation):
+        return self.with_styles(notation=notation)
+
     def latex(self, **kwargs):
-        _a, _b = self.operands
+
+        def _is_ket_variable(expr):
+            from proveit._core_.expression.label.var import Variable
+            if not isinstance(expr, Variable):
+                return False
+            latex = expr.latex()
+            return latex.startswith(r'\lvert') and latex.endswith(r'\rangle')
+        
+        def _strip_ket_delimiters(latex):
+            return latex.replace(r'\lvert', '').replace(r'\rangle', '').strip()
+
+
+        notation = self.get_style('notation', 'standard')
+
+        _a, _b = self.operands.entries
+
+        if (notation == 'dirac' and
+            _is_ket_variable(_a) and
+            _is_ket_variable(_b)):
+
+            left_inner = _strip_ket_delimiters(_a.latex())
+            right_inner = _strip_ket_delimiters(_b.latex())
+
+            return r'\langle %s \mid %s \rangle' % (left_inner, right_inner)
+
         return (r'\left \langle ' + _a.latex() + ', ' 
                 + _b.latex() + r'\right \rangle')
+    
+    def string(self, **kwargs):
+        _a, _b = self.operands.entries
+        def _is_ket_variable(expr):
+            from proveit._core_.expression.label.var import Variable
+            if not isinstance(expr, Variable):
+                return False
+            latex = expr.latex()
+            return latex.startswith(r'\lvert') and latex.endswith(r'\rangle')
+        
+        def _strip_ket_delimiters(latex):
+            return latex.replace(r'\lvert', '').replace(r'\rangle', '').strip()
+
+        if (_is_ket_variable(_a) and _is_ket_variable(_b)):
+
+            left_inner = _strip_ket_delimiters(_a.latex())
+            right_inner = _strip_ket_delimiters(_b.latex())
+
+            return '<%s|%s>' % (left_inner, right_inner)
+
+        return '<' + _a.string() + ', ' + _b.string() + '>'
+    
+    # def string(self, **kwargs):
+    #     _a, _b = self.operands
+    #     return '<' + _a.string() + ', ' + _b.string() + '>'
+    
+    # def latex(self, **kwargs):
+    #     _a, _b = self.operands
+    #     return (r'\left \langle ' + _a.latex() + ', ' 
+    #             + _b.latex() + r'\right \rangle')
+
+    
     
     @equality_prover('shallow_simplified', 'shallow_simplify')
     def shallow_simplification(self, *, must_evaluate=False,
@@ -31,10 +100,13 @@ class InnerProd(Operation):
         <x + y, z> = <x, z> + <y, z>
         <x, y + z> = <x, y> + <x, z>
         '''
-        from proveit.linear_algebra import VecSpaces, ScalarMult, VecAdd
+
+        from proveit.linear_algebra import VecSpaces, ScalarMult, VecAdd, Hspace, VecNeg
         from proveit.linear_algebra.inner_products import (
-                inner_prod_scalar_mult_left, inner_prod_scalar_mult_right,
-                inner_prod_vec_add_left, inner_prod_vec_add_right)
+        inner_prod_scalar_mult_left, inner_prod_scalar_mult_right,
+        inner_prod_vec_add_left, inner_prod_vec_add_right, Norm, inner_prod_in_field, innerprod_normality, inner_prod_vec_neg_left, inner_prod_vec_neg_right)
+        from proveit.physics.quantum import var_ket_psi
+
         _u, _v = self.operands
         try:
             vec_space = VecSpaces.common_known_vec_space((_u, _v))
@@ -43,7 +115,8 @@ class InnerProd(Operation):
             # we have no specific shallow_simplication we can do here.
             return Operation.shallow_simplification(
                     self, must_evaluate=must_evaluate)
-        field = VecSpaces.known_field(vec_space)
+        field = VecSpaces.known_field(vec_space) #additional line Wayne suggested
+        inner_prod_in_field.instantiate({K:field, H:vec_space, x:_u, y:_v})
         simp = None
         if isinstance(_u, ScalarMult):
             simp = inner_prod_scalar_mult_left.instantiate(
@@ -57,10 +130,22 @@ class InnerProd(Operation):
         if isinstance(_v, VecAdd):
             simp = inner_prod_vec_add_right.instantiate(
                     {K:field, H:vec_space, x:_u, y:_v.terms[0], z:_v.terms[1]})
+
+        # our if statements
+        if _u==_v and Equals(Norm(_u), one).proven():
+          simp = innerprod_normality.instantiate({Hspace:vec_space, var_ket_psi:_u})   
+
+        if isinstance(_u, VecNeg):
+            simp= inner_prod_vec_neg_left.instantiate({K:field, H:vec_space, x:_u.operand, y:_v})
+
+        if isinstance(_v, VecNeg):
+            simp= inner_prod_vec_neg_right.instantiate({K:field, H:vec_space, x:_u, y:_v.operand})
+
         if simp is None:
             return Operation.shallow_simplification(
                     self, must_evaluate=must_evaluate)
         if must_evaluate and not is_irreducible_value(simp.rhs):
             return simp.inner_expr().rhs.evaluate()
+            
         return simp
 
