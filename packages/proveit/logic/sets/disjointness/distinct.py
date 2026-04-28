61c114c607de6452e01c507880013d8bcf2bf979
Warren Craft <wdcraft@unm.edu>
2026-04-28 15:35:02 -0600

    AllDistinct() class
    Establish AllDistinct() class to extend Distinct() to a _set_ of elements (rather than the variadic approach taken in Distinct() class).

--- packages/proveit/logic/sets/disjointness/distinct.py
+++ packages/proveit/logic/sets/disjointness/distinct.py
@@ -12,3 +12,37 @@ class Distinct(Function):
     def __init__(self, *elements, styles=None):
         Function.__init__(self, Distinct._operator_, elements,
                           styles=styles)
+
+
+class AllDistinct(Function):
+    '''
+    AllDistinct(S) represents the claim that all elements of collection
+    S are distinct --- i.e.,
+
+        for all e_i, e_j in S, i != j => e_i != e_j.
+
+    For example, AllDistinct({1, 2, 3}) is True, while
+    AllDistinct({1, 2, 2}) is False. The argument S could be abstract
+    (a variable), or S could be a tuple, list, Set, or ExprTuple.
+    This is different from the Distinct() class defined further above,
+    which accepts a variable number of arguments, each of which is
+    an 'item' to be compared to the other arguments.
+
+    '''
+
+    _operator_ = Literal('AllDistinct', r'\textrm{AllDistinct}',
+                         theory=__file__)
+
+    def __init__(self, S, *, styles=None):
+        '''
+        Initialize AllDistinct(S), the claim that all elements of S
+        are distinct.
+        '''
+        # If S is a Python list or tuple,
+        # wrap it so it stays a single unit
+        if isinstance(S, (list, tuple)):
+            from proveit import ExprTuple
+            S = ExprTuple(*S)
+
+        super().__init__(AllDistinct._operator_, [S], styles=styles)
+        self.set = S
