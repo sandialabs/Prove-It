2f1982cf8324d5e603662c9489606a45d1b50cb0
Warren Craft <wdcraft@unm.edu>
2026-06-18 10:30:23 -0600

    Establish KroneckerDelta.shallow_simplification() and other methods
    Establish several KroneckerDelta methods: shallow_simplification(), commutation(), readily_provable_number_set(), deduce_image_set(), and deduce_bounds(). These all depend on recently established/committed theorems.

--- packages/proveit/numbers/functions/delta.py
+++ packages/proveit/numbers/functions/delta.py
@@ -1,4 +1,8 @@
-from proveit import Operation, Literal
+from proveit import (
+        i, j, x, y, equality_prover, Literal, Operation, TransRelUpdater)
+from proveit.logic import Equals, is_irreducible_value, NotEquals
+from proveit.logic.sets import Set
+from proveit.numbers import zero, one
 
 class KroneckerDelta(Operation):
     '''
@@ -22,3 +26,101 @@ class KroneckerDelta(Operation):
     def latex(self, **kwargs):
         return (r'\delta_{' + self.operands[0].string(fence=True) + ', '
                          + self.operands[1].string(fence=True)) + '}'
+
+    @equality_prover('shallow_simplified', 'shallow_simplify')
+    def shallow_simplification(self, *, must_evaluate=False,
+                               **defaults_config):
+        '''
+        Return a proven simplification equation for this KroneckerDelta
+        expression assuming the operands have been simplified.
+        This mostly consists of evaluating the Kronecker delta function
+        d(i,j) for cases where we know that i = j or i ≠ j.
+        '''
+
+        from . import kron_delta_def, kron_delta_image
+        from proveit.logic.sets.enumeration import fold_singleton
+
+        expr = self
+        # For convenience in updating our equation
+        eq = TransRelUpdater(self)
+
+        _op_0 = self.operands[0]
+        _op_1 = self.operands[1]
+
+        # Calling NotEquals(_op_0, _op_1).readily_provable() further
+        # below misses some obvious cases, so we try a proof first
+        try:
+            NotEquals(_op_0, _op_1).prove()
+        except:
+            pass
+        
+        # Now we're ready to instantiate d(i,j) using the evaluation
+        # axiom for any of the following situations:
+        # (1) i == j;
+        # (2) i is readily provably equal to j;
+        # (3) i, j irreducible and not equal
+        if ((_op_0 == _op_1) or (Equals(_op_0, _op_1).readily_provable())
+            or (NotEquals(_op_0, _op_1).readily_provable()) or
+            (is_irreducible_value(_op_0) and is_irreducible_value(_op_1)
+                and _op_0 != _op_1)):
+            eq.update(kron_delta_def.instantiate({i:_op_0, j:_op_1}))
+
+        return eq.relation # Might simply be self=self.
+
+
+
+    @equality_prover('commuted', 'commute')
+    def commutation(self, **defaults_config):
+        '''
+        Deduce that this KroneckerDelta function expression delta(i,j)
+        is equal to the KroneckerDelta function in which the operands
+        have changed order: delta(j, i).
+        '''
+        from . import kron_delta_commutation
+        _i_sub = self.operands[0]
+        _j_sub = self.operands[1]
+        return kron_delta_commutation.instantiate({i:_i_sub, j:_j_sub})
+
+    def readily_provable_number_set(self):
+        '''
+        Return the most restrictive number set we can readily
+        prove contains the evaluation of this KroneckerDelta operation.
+        If we have actual values for the arguments, we can return a
+        singleton set {0} or {1}, otherwise we can return the two-elem
+        set {0, 1}.
+        '''
+        _i = self.operands[0]
+        _j = self.operands[1]
+        if _i == _j: return Set(one)
+        if (is_irreducible_value(_i) and is_irreducible_value(_j)
+           and _i != _j):
+            return Set(zero)
+        return Set(zero, one)
+
+    def deduce_image_set(self, **defaults_config):
+        '''
+        Deduce and return the set membership claim [self in {0,1}].
+        '''
+        from . import kron_delta_def, kron_delta_image
+        from proveit.logic.sets.enumeration import fold_singleton
+        _op_0 = self.operands[0]
+        _op_1 = self.operands[1]
+        if _op_0 == _op_1:
+            kron_delta_eval = kron_delta_def.instantiate({i:_op_0, j:_op_1})
+            return fold_singleton.instantiate({x:self, y:one})
+        if (is_irreducible_value(_op_0) and is_irreducible_value(_op_1)
+           and _op_0 != _op_1):
+            kron_delta_eval = kron_delta_def.instantiate({i:_op_0, j:_op_1})
+            return fold_singleton.instantiate({x:self, y:zero})
+
+        return kron_delta_image.instantiate({i:_op_0, j:_op_1})
+
+    def deduce_bounds(self, **defaults_config):
+        '''
+        Deduce and return the bounding claim that [0 ≤ self ≤ 1].
+        '''
+        from . import kron_delta_bounds
+        _i_sub = self.operands[0]
+        _j_sub = self.operands[1]
+        return kron_delta_bounds.instantiate({i:_i_sub, j:_j_sub})
+
