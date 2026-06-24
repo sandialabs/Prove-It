f10c75aff011ed0137720afb612f453bc510fdb1
Warren Craft <wdcraft@unm.edu>
2026-06-24 09:33:30 -0600

    Add EmptySetLiteral. not_equals_side_effects() method
    Add EmptySetLiteral. not_equals_side_effects() method, intended to be called from the NotEquals.side_effects() method and provide the incidental/side-effect result that Exists_{x}[x in A] when we have 'A ≠ EmptySet'.

--- packages/proveit/logic/sets/empty_set.py
+++ packages/proveit/logic/sets/empty_set.py
@@ -1,4 +1,5 @@
-from proveit import Literal
+from proveit import A, Judgment, Literal
+from proveit.logic import NotEquals
 from proveit.logic.irreducible_value import IrreducibleValue
 
 
@@ -22,3 +23,34 @@ class EmptySetLiteral(Literal, IrreducibleValue):
     def nonmembership_object(self, element):
         from .empty_set_membership import EmptySetNonmembership
         return EmptySetNonmembership(element, self)
+
+    def not_equals_side_effects(self, judgment):
+        '''
+        For a judgment or assumption of the form A ≠ EmptySet,
+        derive the existential Judgment:
+
+          |- Exists_{x} [x in A]
+
+        (i.e., if A is not empty, there must exist an element in A).
+        This side-effect method is called from NotEquals.side_effects().
+        '''
+
+        from . import EmptySet
+        if not isinstance(judgment, Judgment):
+            raise ValueError(
+                    "EmptySet.not_equals_side_effects() expecting 'judgment' "
+                    f"argument to be Judgment but got {judgment}.")
+        if not isinstance(judgment.expr, NotEquals):
+            raise ValueError(
+                    "EmptySet.not_equals_side_effects() expecting "
+                    "'judgment' argument to be an inequality Judgment "
+                    f"but got {judgment}.")
+        if not isinstance(judgment.rhs, EmptySetLiteral):
+            raise ValueError(
+                    "EmptySet.not_equals_side_effects() expecting "
+                    "'judgment' argument have rhs be EmptySet, "
+                    f"but got {judgment.rhs}.")
+        from proveit.logic.sets import non_empty_unfolding
+        _A_sub = judgment.lhs
+        yield (lambda : non_empty_unfolding.instantiate({A:_A_sub}))
+
